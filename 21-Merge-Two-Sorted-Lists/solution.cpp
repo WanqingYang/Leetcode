dcb9c1b40090a560c282b0efda43ed6250eb481c
WanqingYang <latejean@gmail.com>
2016-06-30 15:43:07 -0700

    [21. Merge Two Sorted Lists][Accepted]committed by LeetCode Extension

--- 21-Merge-Two-Sorted-Lists/solution.cpp
+++ 21-Merge-Two-Sorted-Lists/solution.cpp
@@ -8,16 +8,25 @@
  */
 class Solution {
 public:
-    ListNode *mergeTwoLists(ListNode *l1, ListNode *l2) {
-        if(l1 == NULL) return l2;
-        if(l2 == NULL) return l1;
-
-        if(l1->val < l2->val) {
-            l1->next = mergeTwoLists(l1->next, l2);
-            return l1;
-        } else {
-            l2->next = mergeTwoLists(l2->next, l1);
-            return l2;
+    /*
+    题意：合并两个有序链表
+    模拟一轮归并排序
+    */
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        ListNode *dummy = new ListNode(0);
+        ListNode *tmp = dummy;
+        while (l1 != NULL && l2 != NULL) {
+            if (l1->val < l2->val) {
+                tmp->next = l1;
+                l1 = l1->next;
+            } else {
+                tmp->next = l2;
+                l2 = l2->next;
+            }
+            tmp = tmp->next;
         }
+        if (l1 != NULL) tmp->next = l1;
+        else tmp->next = l2;
+        return dummy->next;
     }
 };
