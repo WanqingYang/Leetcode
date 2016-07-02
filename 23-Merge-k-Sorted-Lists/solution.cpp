7f47d3fa78bf473ba0498bddc2f15b7f7d000943
WanqingYang <latejean@gmail.com>
2016-07-01 17:13:36 -0700

    [23. Merge k Sorted Lists][Accepted]committed by LeetCode Extension

--- 23-Merge-k-Sorted-Lists/solution.cpp
+++ 23-Merge-k-Sorted-Lists/solution.cpp
@@ -18,11 +18,11 @@ public:
         else if (NULL == l2) return l1;
         if (l1->val <= l2->val) {
             l1->next = mergeTwoLists(l1->next, l2);
-            //return l1;
+            return l1;
         }
         else {
             l2->next = mergeTwoLists(l1, l2->next);
-            //return l2;
+            return l2;
         }
     }
     ListNode *mergeKLists(vector<ListNode *> &lists) {
