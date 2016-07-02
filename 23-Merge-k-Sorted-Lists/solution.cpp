c1119059235c2e3330860f34d3d49e064423141e
WanqingYang <latejean@gmail.com>
2016-07-01 17:37:51 -0700

    [23. Merge k Sorted Lists][Compile Error]committed by LeetCode Extension

--- 23-Merge-k-Sorted-Lists/solution.cpp
+++ 23-Merge-k-Sorted-Lists/solution.cpp
@@ -13,28 +13,28 @@ public:
      * @param lists: a list of ListNode
      * @return: The head of one sorted list.
      */
-    ListNode *mergeTwoLists(ListNode* l1, ListNode* l2) {
-        if (NULL == l1) return l2;
-        else if (NULL == l2) return l1;
-        if (l1->val <= l2->val) {
-            l1->next = mergeTwoLists(l1->next, l2);
-            return l1;
+
+    ListNode *mergeKLists(vector<ListNode *> &lists) {
+        if(lists.empty()){return NULL}
+        int len = lists.size();
+        while(len > 1){
+            for(i = 0; i < len / 2; i++){
+                lists[i] = mergeTwoLists(lists[i], lists[len - i - 1])
+            }
+            len = (len + 1)/2;
         }
-        else {
-            l2->next = mergeTwoLists(l1, l2->next);
+        return lists.front();
+    }
+    
+    ListNode *mergeTwoLists(ListNode *l1, ListNode *l2){
+        if(l1 == NULL){return l2}
+        else if(l2 == NULL){return l1}
+        if(l1->val < l2->val){
+            l1->next = mergeTwoLists(l1->next, l2)
+            return l1;
+        } else{
+            l2->next = mergeTwoLists(l2->next, l2)
             return l2;
         }
-    }
-    ListNode *mergeKLists(vector<ListNode *> &lists) {
-        if (lists.empty()) return NULL;
-        int len = lists.size();
-        while (len > 1) {
-            for (int i = 0; i < len / 2; ++i) {
-                lists[i] = mergeTwoLists(lists[i], lists[len - 1 - i]);
-            }
-            len = (len + 1) / 2;
-        }
-
-        return lists.front();
     }
 };
