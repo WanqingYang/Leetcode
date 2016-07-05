6594edb6ab77ff895901556dcda111ce84bbf5ac
WanqingYang <latejean@gmail.com>
2016-07-05 12:12:36 -0700

    [206. Reverse Linked List][Accepted]committed by LeetCode Extension

---
+++ 206-Reverse-Linked-List/solution.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+/*class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if(head->next == NULL){return head;}
+        
+        ListNode* pre = head->next;
+        //head->next->next = head->next;
+        head->next->next = reverseList(pre->next);
+        pre->next = head;
+        return pre;
+    }
+};*/
+
+class Solution{
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* pre = NULL;
+        ListNode* temp;
+        while(head != NULL){
+            temp = head->next;
+            head->next = pre;
+            pre = head;
+            head = temp;
+        }
+        return pre;
+    }
+    
+};
