4c9478d3bc1f945746dc43475f39e1293b886ada
WanqingYang <latejean@gmail.com>
2016-07-05 16:21:09 -0700

    [92. Reverse Linked List II][Compile Error]committed by LeetCode Extension

--- 92-Reverse-Linked-List-II/solution.cpp
+++ 92-Reverse-Linked-List-II/solution.cpp
@@ -10,20 +10,62 @@
 class Solution {  
 public:
     ListNode *reverseBetween(ListNode *head, int m, int n) {
-    if(m==n)return head;
-	n-=m;
-    ListNode prehead(0);
-    prehead.next=head;
-    ListNode* pre=&prehead;
-    while(--m)pre=pre->next;        
-    ListNode* pstart=pre->next;
-    while(n--)
-    {
-        ListNode *p=pstart->next;
-        pstart->next=p->next;
-        p->next=pre->next;
-        pre->next=p;
-    }
-    return prehead.next;
+        
     }
 };
+
+class ListNode *reverseBetween(ListNode *head, int m, int n) {
+    
+    //handle case for empty list
+    if(head == NULL){
+        return NULL;
+    }
+    //handle special case for one node list
+    if(head->next == NULL){
+        return head;
+    }
+    //handle input values of m & n , if m==n - no need to do anything 
+    if(m >= n){
+        return head;
+    }
+
+    struct ListNode * prev  = head;    
+    struct ListNode * curr  = head;
+    struct ListNode * next  = head;
+
+    struct ListNode *mTh   = NULL; //mTh node
+    struct ListNode *nTh   = NULL; //nTh node         
+    struct ListNode *mPrev = NULL; // node just previous to mTh node
+    struct ListNode *nNext = NULL; // node next to the nTh node
+    
+    for(int i = 1 ; curr != NULL ; i++){
+
+        next = curr->next ;
+
+        if(i == m){
+           //when we reach the mTh node, save mTh and mPrevious
+            mPrev = prev ; 
+            mTh   = curr ; 
+        }
+        if(i> m && i <= n){    
+             //revese links if we fall within the m and n range (include n)
+             curr->next = prev;
+        }
+        if(i == n ){
+           //when we reach the nTh node, save nTh and nNext
+            nNext = next ;
+            nTh   = curr ;  
+        }
+        prev = curr; 
+        curr = next;
+    }
+    if(m == 1 ){
+        //handle special case if head needs to be changed since m==1 
+        mTh->next = nNext;
+        head = nTh;
+    }else{
+        mPrev->next  = nTh ; 
+        mTh->next = nNext; 
+    }
+    return head;
+}
