c36cd37c40ad9b70147c40fe73798f8ee2efbb36
WanqingYang <latejean@gmail.com>
2016-07-05 11:09:13 -0700

    [24. Swap Nodes in Pairs][Accepted]committed by LeetCode Extension

--- 24-Swap-Nodes-in-Pairs/solution.cpp
+++ 24-Swap-Nodes-in-Pairs/solution.cpp
@@ -8,14 +8,16 @@
  */
 class Solution {
 public:
-ListNode* swapPairs(ListNode* head) {
-    ListNode **pp = &head, *a, *b;
-    while ((a = *pp) && (b = a->next)) {
-        a->next = b->next;
-        b->next = a;
-        *pp = b;
-        pp = &(a->next);
+    ListNode* swapPairs(ListNode* head) {
+        if(head == NULL)
+            return NULL;
+        if(head->next == NULL)
+            return head;
+        
+        ListNode* next = head->next;
+        head->next = swapPairs(next->next);
+        next->next = head;
+        
+        return next;
     }
-    return head;
-}
 };
