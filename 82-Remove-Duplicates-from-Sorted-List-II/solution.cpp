90e36dad289e1688ccc20ef2b0ef9317bc0c2129
WanqingYang <latejean@gmail.com>
2016-07-06 18:50:42 -0700

    [82. Remove Duplicates from Sorted List II][Compile Error]committed by LeetCode Extension

---
+++ 82-Remove-Duplicates-from-Sorted-List-II/solution.cpp
@@ -0,0 +1,63 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *deleteDuplicate(ListNode *head){        
+         if(head == nullptr){return head;}
+          ListNode dummy(INT_MIN);
+          dummy.next = head;
+          ListNode *prev = &dummy, *cur = head;
+          while(cur){
+             bool duplicated = false;
+             while(cur->next && cur->next->val == cur->val){
+                     duplicated = true;
+                     ListNode *tmp = cur;
+                     cur = cur->next;
+                     delete tmp;
+                   }
+             if(duplicated){
+                  ListNode *tmp = cur;
+                  cur = cur->next;
+                  delete tmp;
+                  continue;
+               }
+               prev->next = cur;
+               prev = prev->next;
+               cur = cur->next;
+           }
+            prev->next = cur;
+            return dummy.next;leteDuplicates(ListNode* head) {
+          if(head == nullptr)
+             return head;
+
+          ListNode dummy(INT_MIN);
+          dummy.next = head;
+          ListNode *prev = &dummy, *cur = head;
+          while(cur){
+             bool duplicated = false;
+             while(cur->next && cur->next->val == cur->val){
+                     duplicated = true;
+                     ListNode *tmp = cur;
+                     cur = cur->next;
+                     delete tmp;
+                   }
+             if(duplicated){
+                  ListNode *tmp = cur;
+                  cur = cur->next;
+                  delete tmp;
+                  continue;
+               }
+               prev->next = cur;
+               prev = prev->next;
+               cur = cur->next;
+           }
+            prev->next = cur;
+            return dummy.next;
+    }
+};
