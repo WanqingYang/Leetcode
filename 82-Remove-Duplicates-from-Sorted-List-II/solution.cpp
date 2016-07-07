6b0adaabd2c73fba5430dbaf1b9fdf882764376f
WanqingYang <latejean@gmail.com>
2016-07-06 18:52:49 -0700

    [82. Remove Duplicates from Sorted List II][Compile Error]committed by LeetCode Extension

--- 82-Remove-Duplicates-from-Sorted-List-II/solution.cpp
+++ 82-Remove-Duplicates-from-Sorted-List-II/solution.cpp
@@ -8,31 +8,7 @@
  */
 class Solution {
 public:
-    ListNode *deleteDuplicate(ListNode *head){        
-         if(head == nullptr){return head;}
-          ListNode dummy(INT_MIN);
-          dummy.next = head;
-          ListNode *prev = &dummy, *cur = head;
-          while(cur){
-             bool duplicated = false;
-             while(cur->next && cur->next->val == cur->val){
-                     duplicated = true;
-                     ListNode *tmp = cur;
-                     cur = cur->next;
-                     delete tmp;
-                   }
-             if(duplicated){
-                  ListNode *tmp = cur;
-                  cur = cur->next;
-                  delete tmp;
-                  continue;
-               }
-               prev->next = cur;
-               prev = prev->next;
-               cur = cur->next;
-           }
-            prev->next = cur;
-            return dummy.next;leteDuplicates(ListNode* head) {
+ ListNode *deleteDuplicate(ListNode *head){
           if(head == nullptr)
              return head;
 
@@ -59,5 +35,5 @@ public:
            }
             prev->next = cur;
             return dummy.next;
-    }
+     }
 };
