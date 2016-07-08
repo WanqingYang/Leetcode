ba760483df322abe8f3982246b8fdb0882ed9997
WanqingYang <latejean@gmail.com>
2016-07-08 00:37:43 -0700

    [141. Linked List Cycle][Runtime Error]committed by LeetCode Extension

---
+++ 141-Linked-List-Cycle/solution.cpp
@@ -0,0 +1,20 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        ListNode *fast, *slow = head;
+        while(fast != NULL){
+            slow = slow -> next;
+            fast = fast -> next -> next;
+            if(fast == slow){return true;}
+        }
+        return false;
+    }
+};
