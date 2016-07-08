7c4da6ddc5e05da0018f0426e82e87d7820d83bc
WanqingYang <latejean@gmail.com>
2016-07-08 00:50:43 -0700

    [141. Linked List Cycle][Time Limit Exceeded]committed by LeetCode Extension

--- 141-Linked-List-Cycle/solution.cpp
+++ 141-Linked-List-Cycle/solution.cpp
@@ -9,7 +9,7 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        if(head == NULL){return false;}
+        if(head == NULL || head -> next == NULL){return false;}
         ListNode *fast = head;
         while(fast -> next -> next != NULL){
             fast = fast -> next -> next;
