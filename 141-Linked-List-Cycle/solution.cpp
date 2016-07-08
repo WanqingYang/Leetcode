321da13745f271fccc7698d9d74fd890e8df0b95
WanqingYang <latejean@gmail.com>
2016-07-08 00:46:42 -0700

    [141. Linked List Cycle][Runtime Error]committed by LeetCode Extension

--- 141-Linked-List-Cycle/solution.cpp
+++ 141-Linked-List-Cycle/solution.cpp
@@ -11,12 +11,10 @@ public:
     bool hasCycle(ListNode *head) {
         if(head == NULL || head -> next == NULL){return false;}
         ListNode *fast, *slow = head;
-        while(fast != NULL){
+        while(slow -> next != NULL &&fast -> next -> next != NULL){
             slow = slow -> next;
             fast = fast -> next -> next;
-            while(fast != NULL){
-                if(fast == slow){return true;}
-            }
+            if(fast == slow){return true;}
         }
         return false;
     }
