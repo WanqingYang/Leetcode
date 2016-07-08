975b04705b1e225d43378687427f854346a696e1
WanqingYang <latejean@gmail.com>
2016-07-08 10:41:24 -0700

    [61. Rotate List][Runtime Error]committed by LeetCode Extension

--- 61-Rotate-List/solution.cpp
+++ 61-Rotate-List/solution.cpp
@@ -10,16 +10,16 @@ class Solution {
 public:
     ListNode* rotateRight(ListNode* head, int k) {
       if(head == nullptr) return head;
-      ListNode *cur = head, *new, *last;
+      ListNode *cur = head, *new1, *last;
       for(int i = 1; i < k && cur; ++i)
               cur = cur->next;
 
-      new = cur->next; last = new;
+      new1 = cur->next; last = new1;
       cur->next = nullptr;
       while(last->next)
              last = last->next;
       last->next = head;
 
-      return new;
+      return new1;
     }
 };
