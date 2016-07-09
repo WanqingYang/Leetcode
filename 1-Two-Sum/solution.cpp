362f6d2fbe213addb01c7fb7af5d35ca1ed1d073
WanqingYang <latejean@gmail.com>
2016-07-09 14:17:01 -0700

    [1. Two Sum][Accepted]committed by LeetCode Extension

---
+++ 1-Two-Sum/solution.cpp
@@ -0,0 +1,22 @@
+class Solution {
+    
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map <int, int> find_index;
+        unsigned int i;
+        vector<int> result;
+        int sec_num;
+        
+        for(i = 0; i < nums.size(); i++)
+        {
+            sec_num = target - nums[i];
+            if(find_index.find(sec_num) != find_index.end())
+            {
+                result.push_back(find_index[sec_num]);
+                result.push_back(i);
+            }
+           find_index[nums[i]] = i; 
+        }
+        return result;
+    }
+};
