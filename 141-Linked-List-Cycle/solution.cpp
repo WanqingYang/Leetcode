6cf01d9cd148faaf888f2ba1e67dcfaca408e7bf
WanqingYang <latejean@gmail.com>
2016-07-11 11:50:23 -0700

    [141. Linked List Cycle][Accepted]committed by LeetCode Extension

--- 141-Linked-List-Cycle/solution.cpp
+++ 141-Linked-List-Cycle/solution.cpp
@@ -10,17 +10,11 @@ class Solution {
 public:
     bool hasCycle(ListNode *head) {
         if(head == NULL || head -> next == NULL){return false;}
-        ListNode *fast = head, *slow = head, *entry = head;
+        ListNode *fast = head, *slow = head;
         while(fast->next != NULL && fast -> next -> next != NULL){
             slow = slow -> next;
             fast = fast -> next ->next;
-            if(fast == slow){
-                while(entry != slow){
-                    slow = slow -> next;
-                    entry = entry -> next;
-                }
-                return  entry;
-            }
+            if(fast == slow){return true;}
         }
         return false;
     }
