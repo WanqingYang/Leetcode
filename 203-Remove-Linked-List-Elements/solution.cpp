c8f1ee117a30cf76e73b6a86e8b6e44f2b10cf2d
WanqingYang <latejean@gmail.com>
2016-07-11 16:18:26 -0700

    [203. Remove Linked List Elements][Accepted]committed by LeetCode Extension

--- 203-Remove-Linked-List-Elements/solution.cpp
+++ 203-Remove-Linked-List-Elements/solution.cpp
@@ -9,6 +9,17 @@
 class Solution {
 public:
     ListNode* removeElements(ListNode* head, int val) {
-        
+        if(head = NULL){return head;}
+        ListNode *dummy = head;
+        while(head){
+            if(head -> val == val){
+                ListNode* temp = head;
+                head = head -> next;
+                delete temp;
+            }else{
+                head = head -> next;
+            }
+        }
+        return dummy;
     }
 };
