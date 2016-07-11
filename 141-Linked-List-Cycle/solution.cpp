d270c66701fe634abcdfd2c6efcf6a23c3ab37c8
WanqingYang <latejean@gmail.com>
2016-07-11 11:48:56 -0700

    [141. Linked List Cycle][Compile Error]committed by LeetCode Extension

--- 141-Linked-List-Cycle/solution.cpp
+++ 141-Linked-List-Cycle/solution.cpp
@@ -10,11 +10,17 @@ class Solution {
 public:
     bool hasCycle(ListNode *head) {
         if(head == NULL || head -> next == NULL){return false;}
-        ListNode *fast = head, *slow = head;
+        ListNode *fast = head, *slow = head, *entry = head;
         while(fast->next != NULL && fast -> next -> next != NULL){
             slow = slow -> next;
             fast = fast -> next ->next;
-            if(fast == slow){return true;}
+            if(fast == slow){
+                while(entry != slow){
+                    slow = slow -> next;
+                    entry = entry -> next;
+                }
+                return  entry;
+            }
         }
         return false;
     }
