2c532958543c22fc3ccdfdb72993e6e83e2705d4
WanqingYang <latejean@gmail.com>
2016-07-13 15:28:12 -0700

    [138. Copy List with Random Pointer][Accepted]committed by LeetCode Extension

--- 138-Copy-List-with-Random-Pointer/solution.cpp
+++ 138-Copy-List-with-Random-Pointer/solution.cpp
@@ -6,6 +6,7 @@
  *     RandomListNode(int x) : label(x), next(NULL), random(NULL) {}
  * };
  */
+ /*
 class Solution {
 public:
     RandomListNode *copyRandomList(RandomListNode *head) {
@@ -30,4 +31,31 @@ public:
 
     return newHead;
     }
+};*/
+class Solution{
+public:
+    RandomListNode *copyRandomList(RandomListNode *head) {
+    RandomListNode *newHead, *l1, *l2;
+    if (head == NULL) return NULL;
+
+    for (l1 = head; l1 != NULL; l1 = l1->next) {
+        l2 = new RandomListNode(l1->label);
+        l2->next = l1->random;
+        l1->random = l2;
+    }
+    
+    newHead = head->random;
+    for (l1 = head; l1 != NULL; l1 = l1->next) {
+        l2 = l1->random;
+        l2->random = l2->next ? l2->next->random : NULL;
+    }
+    
+    for (l1 = head; l1 != NULL; l1 = l1->next) {
+        l2 = l1->random;
+        l1->random = l2->next;
+        l2->next = l1->next ? l1->next->random : NULL;
+    }
+
+    return newHead;
+}
 };
