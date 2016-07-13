5703e35af7cef2959440f7bb0868d58694462fee
WanqingYang <latejean@gmail.com>
2016-07-13 12:31:08 -0700

    [109. Convert Sorted List to Binary Search Tree][Accepted]committed by LeetCode Extension

--- 109-Convert-Sorted-List-to-Binary-Search-Tree/solution.cpp
+++ 109-Convert-Sorted-List-to-Binary-Search-Tree/solution.cpp
@@ -15,7 +15,7 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
- //Bottom up
+/* //Bottom up
 class Solution {
 public:
     TreeNode* sortedListToBST(ListNode* head) {
@@ -42,6 +42,35 @@ public:
         root -> right = rightChild;
         return root;
     }
-};
+};*/
 
 //Top down
+class Solution {
+public:
+    TreeNode *sortedListToBST(ListNode *head)
+    {
+    	return sortedListToBST( head, NULL );
+    }
+    
+private:
+    TreeNode *sortedListToBST(ListNode *head, ListNode *tail)
+    {
+    	if( head == tail )
+    		return NULL;
+    	if( head->next == tail )    // 
+    	{	
+    		TreeNode *root = new TreeNode( head->val );
+    		return root;
+    	}
+    	ListNode *mid = head, *temp = head;
+    	while( temp != tail && temp->next != tail )    // 寻找中间节点
+    	{
+    		mid = mid->next;
+    		temp = temp->next->next;
+    	}
+    	TreeNode *root = new TreeNode( mid->val );
+    	root->left = sortedListToBST( head, mid );
+    	root->right = sortedListToBST( mid->next, tail );
+    	return root;
+    }
+};
