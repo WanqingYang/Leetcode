f35838b0f0f3d9fef9abaceb021880eec212c57c
WanqingYang <latejean@gmail.com>
2016-07-13 21:49:02 -0700

    [100. Same Tree][Runtime Error]committed by LeetCode Extension

---
+++ 100-Same-Tree/solution.cpp
@@ -0,0 +1,20 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+ //use recursion:
+ //base case: if p and q are both NULL, return(if they are the same)
+ //recursion: if p,q's value, their left tree, their right tree, are the same, 
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if(p == NULL && q == NULL){return p == q;}
+        return(p -> val == q -> val && isSameTree(p -> left, q -> left) 
+        && isSameTree(p -> right, q -> right));
+    }
+};
