cf4c7e4382b3455ebeed779d83688ecf9d3648a6
WanqingYang <latejean@gmail.com>
2016-07-14 01:35:37 -0700

    [101. Symmetric Tree][Accepted]committed by LeetCode Extension

--- 101-Symmetric-Tree/solution.cpp
+++ 101-Symmetric-Tree/solution.cpp
@@ -10,8 +10,14 @@
 class Solution {
 public:
     bool isSymmetric(TreeNode* root) {
-        if(root == NULL || (root -> right == NULL && root -> left == NULL)){return true;}
-        if(root -> left -> val == root -> right -> val){return true;}
-        return(isSymmetric(root -> left) && isSymmetric(root -> right));
+        if(root == NULL){return true;}
+        return helper(root -> left, root -> right);
+    }
+    
+    bool helper(TreeNode *p, TreeNode *q){
+        if(p == NULL && q == NULL){return true;}
+        else if(p == NULL || q == NULL){return false;}
+        if(p -> val != q -> val){return false;}
+        return helper(p -> left, q -> right) && helper(p -> right, q -> left);
     }
 };
