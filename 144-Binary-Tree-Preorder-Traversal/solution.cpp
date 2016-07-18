82f737b44dc5004e4ccf0c86cfd6c5e67699b9da
WanqingYang <latejean@gmail.com>
2016-07-18 09:24:17 -0700

    [144. Binary Tree Preorder Traversal][Accepted]committed by LeetCode Extension

--- 144-Binary-Tree-Preorder-Traversal/solution.cpp
+++ 144-Binary-Tree-Preorder-Traversal/solution.cpp
@@ -7,7 +7,7 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
- //recursion
+/* //recursion
 class Solution {
 public:
     vector<int> preorderTraversal(TreeNode* root) {
@@ -22,9 +22,28 @@ public:
         helper(p -> left, v);
         helper(p -> right, v);
     }
-};
+};*/
 
 //iteration
+class Solution{
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> v;
+        if(!root){return v;}
+        stack<TreeNode*> s; //must use stack, for it is depth first search
+        TreeNode* cur = root;
+        s.push(root);
+        
+        while(!s.empty()){
+            cur = s.top();
+            s.pop();
+            v.push_back(cur -> val);
+            if(cur -> right){s.push(cur -> right);}//right store earlier, then left, for last in first
+            if(cur -> left){s.push(cur -> left);}//out
+        }
+        return v;
+    }
+};
 
 
 //Morris Traversal
