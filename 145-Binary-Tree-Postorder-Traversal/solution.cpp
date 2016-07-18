5d70d286d86d94316c242f1f2973c5d1293a95b3
WanqingYang <latejean@gmail.com>
2016-07-18 13:01:18 -0700

    [145. Binary Tree Postorder Traversal][Accepted]committed by LeetCode Extension

--- 145-Binary-Tree-Postorder-Traversal/solution.cpp
+++ 145-Binary-Tree-Postorder-Traversal/solution.cpp
@@ -7,7 +7,7 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
- //iteration
+/* //iteration
 class Solution {
 public:
     vector<int> postorderTraversal(TreeNode* root) {
@@ -21,7 +21,7 @@ public:
                 cur = cur -> left;
             }else{
                 TreeNode *top = s.top();
-                if(top -> right/*need print*/ && lastNode != top -> right/*if back to root*/){
+                if(top -> right&& lastNode != top -> right){ //if need print&&back to root
                     cur = top -> right;
                 }else{
                     v.push_back(top -> val);
@@ -29,11 +29,10 @@ public:
                     s.pop();
                 }
             }
-
         }
         return v;
     }
-};
+};*/
 
 /*//recursion
 class Solution {
@@ -52,10 +51,62 @@ public:
     }
 };*/
 
-/*//Morris Traversal
+//Morris Traversal
+//需要建立一个临时节点dummy，令其左孩子是root。
+//并且还需要一个子过程，就是倒序输出某个节点之间路径上的各个节点。
 class Solution {
 public:
-    vector<int> postorderTraversal(TreeNode* root) {
-        
+    void reverseNodes(TreeNode *start, TreeNode *end){
+        if(start == end){return;}
+        TreeNode *x = start, *y = start -> right, *z;
+        while(x != end){
+            z = y -> right;
+            y -> right = x;
+            x = y;
+            y = z;
+        }
     }
-};*/
+    
+    void reverseAddNodes(TreeNode *start, TreeNode *end, vector<int> &v){
+        reverseNodes(start, end);
+        TreeNode *p = end;
+        /*while(p != start){    //do not use this, if p == start at the first time, no push_back
+            v.push_back(p -> val);
+            p = p -> right;
+        }*/
+        while(true){
+            v.push_back(p -> val);
+            if(p == start){break;}
+            p = p -> right;
+        }
+        reverseNodes(end, start);
+    }
+
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> v;
+        TreeNode dummy(0);
+        dummy.left = root;
+        TreeNode *cur = &dummy, *pre = NULL;
+        
+        while(cur){
+            if(cur -> left == NULL){
+                cur = cur -> right;
+            }else{
+                //find the inorder predecessor
+                pre = cur -> left;
+                while(pre -> right != NULL && pre -> right != cur){
+                    pre = pre -> right;
+                }
+                if(pre -> right == NULL){
+                    pre -> right = cur;
+                    cur = cur -> left;
+                }else{
+                    reverseAddNodes(cur -> left, pre, v); //reverse from cur->left and pre,add to vector using the reversed order
+                    pre -> right = NULL;
+                    cur = cur -> right;
+                }
+            }
+        }
+        return v;
+    }
+};
