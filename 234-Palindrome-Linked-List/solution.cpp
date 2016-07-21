0669a272cd109331ede3b25ce12eca07bf461c37
WanqingYang <latejean@gmail.com>
2016-07-21 10:52:47 -0700

    [234. Palindrome Linked List][Accepted]committed by LeetCode Extension

--- 234-Palindrome-Linked-List/solution.cpp
+++ 234-Palindrome-Linked-List/solution.cpp
@@ -6,33 +6,41 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
-/*class Solution {
+ //reverse half of the list
+class Solution {
 public:
     bool isPalindrome(ListNode* head) {
         if(head == NULL || head -> next == NULL){return true;}
-        ListNode *pre = NULL, *head_copy = head, *temp;
-        while(head){
-            temp = head -> next;
-            head -> next = pre;
-            pre = head;
-            head = temp;
+        ListNode* fast = head, *slow = head;
+        while(fast -> next && fast -> next -> next){
+            fast = fast -> next -> next;
+            slow = slow -> next;
         }
-        while(pre){
-            if(pre -> val == head_copy -> val){
-                pre = pre -> next;
-                head_copy = head_copy -> next;
-            }else{
-                return false;
-            }
+        slow -> next = reverse(slow -> next);//odd list, compare middlenode's left and right
+        slow = slow -> next; //right half start at this pointer, do not forget
+        while(slow){
+            if(slow -> val != head -> val){return false;}
+            slow = slow -> next;
+            head = head -> next;
         }
         return true;
     }
-};*/
-
-
-class Solution {
+    
+    ListNode* reverse(ListNode* rightHead){
+        ListNode *pre = NULL, *temp = NULL;
+        while(rightHead){
+            temp = rightHead -> next;
+            rightHead -> next = pre;
+            pre = rightHead;
+            rightHead = temp;
+        }
+        return pre;
+    }
+};
+//recursion
+/*class Solution {
 public:
-    ListNode* temp;
+    ListNode* temp;   //slow pointer, when fast goes to end(null), slow start from head;
     bool isPalindrome(ListNode* head) {
         temp = head;
         return check(head);
@@ -40,8 +48,8 @@ public:
     
     bool check(ListNode* p) {
         if (p == NULL) {return true;}
-        bool isPal = check(p->next) && (temp->val == p->val);
+        bool isPal = check(p->next) && (temp->val == p->val); //??how it worked
         temp = temp->next;
         return isPal;
     }
-};
+};*/
