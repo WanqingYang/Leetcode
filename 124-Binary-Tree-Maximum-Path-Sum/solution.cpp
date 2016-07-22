7de1cf6fa9957f152c13595694e4fb8dd4fafa77
WanqingYang <latejean@gmail.com>
2016-07-21 17:09:15 -0700

    [124. Binary Tree Maximum Path Sum][Accepted]committed by LeetCode Extension

--- 124-Binary-Tree-Maximum-Path-Sum/solution.cpp
+++ 124-Binary-Tree-Maximum-Path-Sum/solution.cpp
@@ -7,13 +7,44 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+ 
+class Solution {
+    int sum; //declare here, cause need it in both function
+public:
+    int maxPathSum(TreeNode* root) {
+        sum = INT_MIN;
+        helper(root);
+        return sum;
+    }
+    
+    /*** return the max-value-ended-at-cur-node ***/
+    int helper(TreeNode* cur){
+        if(!cur){return 0;}
+        int left = max(0, helper(cur -> left));
+        int right = max(0, helper(cur -> right));
+        /*** key parts : embedding the max-value-find in the recursion process ***/
+        sum = max(sum, left + right + cur -> val);
+        /*** get the max-value-ended-at-cur ***/
+        return max(left, right) + cur -> val;
+    }
+};
+ 
+ /*
 class Solution{
 public:
     int maxPathSum(TreeNode* root){
-        return unitTri(root, 0);
+        if(root && !(root -> left) && !(root ->right)){
+            return root -> val;
+        }
+        return unit(root);
+    }
+    int unit(TreeNode* cur){
+        if(!cur){return 0;}
+        if(cur -> val < 0){return 0;}
+        return cur -> val + unit(cur -> left) + unit(cur -> right);
     }
     
-    int unitTri(TreeNode* cur, int sum){
+    /*int unitTri(TreeNode* cur, int sum){
         if(!cur){return 0;}
         if(cur -> val < 0){return 0;}
         //cur is not null && cur -> val >= 0 
@@ -29,9 +60,7 @@ public:
             }
         }
         return unitTri(cur -> left, sum) + unitTri(cur -> right, sum);
-    }
-
-};
+    }*/
  
 /*class Solution {
 public:
