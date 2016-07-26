561498539d85ddccce64905cba4b6c44ec02b28e
WanqingYang <latejean@gmail.com>
2016-07-26 09:21:14 -0700

    [1. Two Sum][Accepted]committed by LeetCode Extension

--- 1-Two-Sum/solution.cpp
+++ 1-Two-Sum/solution.cpp
@@ -1,18 +1,25 @@
+/**IDEA:use unordered_map to store array, set second_num = target-first_num, which is i;
+ * NOTICE: the second_num is the 1st in return array, 
+ * cause you store it first, then find it through the other num.
+ * 
+ * TIME:O(N)
+ **/
+
+
 class Solution {
-    
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         unordered_map <int, int> find_index;
-        unsigned int i;
+        //unsigned int i;
         vector<int> result;
-        int sec_num;
+        int fir_num; //first number in nums, find it through i
         
-        for(i = 0; i < nums.size(); i++)
+        for(int i = 0; i < nums.size(); i++)
         {
-            sec_num = target - nums[i];
-            if(find_index.find(sec_num) != find_index.end())
+            fir_num = target - nums[i];
+            if(find_index.find(fir_num) != find_index.end())
             {
-                result.push_back(find_index[sec_num]);
+                result.push_back(find_index[fir_num]);
                 result.push_back(i);
             }
            find_index[nums[i]] = i; 
