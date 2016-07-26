b4e8c90497c817ffca56a4628c97864b746374ba
WanqingYang <latejean@gmail.com>
2016-07-26 10:37:35 -0700

    [4. Median of Two Sorted Arrays][Accepted]committed by LeetCode Extension

---
+++ 4-Median-of-Two-Sorted-Arrays/solution.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        int m = nums1.size(), n = nums2.size();
+        if(m > n) {return findMedianSortedArrays(nums2, nums1);} //????deal with when nums2 is empty
+        int i, j, imin = 0, imax = m, half_length = (m + n + 1) / 2,
+        max_left, min_right;
+        
+        while(imin <= imax)  // ???????why
+        {   //i = (imin & imax) + ((imin ^ imax) >> 1);  // why can write like this????
+            i = (imin + imax)/2;
+            j = half_length - i;
+            if(j > 0 && i < m && nums1[i] < nums2[j - 1]){  //i is too small, increase i
+                imin = i + 1;
+            }
+            else if(i > 0 && j < n && nums1[i - 1] > nums2[j]){  //i is too big, decrease it 
+                imax = i - 1;
+            }
+            else break;                //avoid dead loop
+        }
+        
+        //i is perfect
+        if(i == 0){max_left = nums2[j - 1];}
+        else if(j == 0){max_left = nums1[i - 1];}
+        else{max_left = max(nums1[i - 1], nums2[j - 1]);}
+        if((m + n) % 2 == 1){return double(max_left);}  // m+n is odd
+                
+        if(i == m){min_right = nums2[j];}
+        else if( j == n){min_right = nums1[i];}
+        else{min_right =  min(nums1[i], nums2[j]);}
+        return double((max_left + min_right)/2.0);
+    }
+};
