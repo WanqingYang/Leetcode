b68c9d53c055b479a93a8178bdaf645a960bdb5c
WanqingYang <latejean@gmail.com>
2016-08-07 16:29:37 -0700

    [108. Convert Sorted Array to Binary Search Tree][Runtime Error]committed by LeetCode Extension

--- 108-Convert-Sorted-Array-to-Binary-Search-Tree/solution.cpp
+++ 108-Convert-Sorted-Array-to-Binary-Search-Tree/solution.cpp
@@ -10,14 +10,14 @@
 class Solution {
 public:
     TreeNode* sortedArrayToBST(vector<int>& nums) {
-        if (nums.size() == 0)
+        if (nums.size() == 0)  //if not write this line, num.size is zero, -1 is not unsigned, cannot pass to helper
             return NULL;
         return helper(nums, 0, nums.size() - 1);
     }
     
     TreeNode *helper(vector<int> &nums, int start, int end){
         int _size = end -  start;
-        if(_size < 0){return NULL;}
+        //if(_size < 0){return NULL;}
         if(_size == 0){return new TreeNode(nums[start]);}
         int mid = (start + end)/2;
         TreeNode* root = new TreeNode(nums[mid]);
