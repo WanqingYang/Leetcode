fe4a4b940fbd188e25b9349b7f7c4daa442e64bc
WanqingYang <latejean@gmail.com>
2016-08-10 15:54:45 -0700

    [116. Populating Next Right Pointers in Each Node][Compile Error]committed by LeetCode Extension

--- 116-Populating-Next-Right-Pointers-in-Each-Node/solution.cpp
+++ 116-Populating-Next-Right-Pointers-in-Each-Node/solution.cpp
@@ -11,14 +11,17 @@
  * If there is no next right node, the next pointer should be set to NULL.
  * Initially, all next pointers are set to NULL.
  **/
+ /*
+ //my solution; too long and wrong
 class Solution {
 public:
     void connect(TreeLinkNode *root) {
         queue<TreeLinkNode*> store;
         TreeLinkNode *cur = root;
         if(cur){store.push(cur);}
+        else{return;}
         
-        while(cur -> left && cur -> next){//if left exist, right must exist, for the tree is perfect
+        while(cur -> left){//if left exist, right must exist, for the tree is perfect
             int _size = store.size();
             for(int i = 0; i < _size - 1; i++){
                 //get the first element of queue, connect it to the next pointer to its next right node
@@ -32,11 +35,33 @@ public:
                 }
             }
             TreeLinkNode* last = store.front();
+            //last -> next = NULL;
             if(last -> left){
                 store.push(last -> left);
                 store.push(last -> right);
             }
             store.pop();
+            cur = cur -> left;
+        }
+    }
+};*/
+
+class Solution {
+public:
+    void connect(TreeLinkNode *root){
+        if(!root){return;}
+        TreeLinkNoden *pre = root, *cur = NULL;
+        
+        while(pre -> left){ //if pre -> left == NULL, means it gets the last line
+            cur = pre;
+            while(cur){ //if cur == NULL, means it gets the last node in this line
+                cur -> left -> next = cur -> right;
+                if(cur -> next){ //if cur -> next exist, then continuing connect; if not exist, it gets the last node
+                    cur -> right -> next = cur -> next -> left;
+                }
+                cur = cur -> next;
+            }
+            pre = pre -> left;
         }
     }
 };
