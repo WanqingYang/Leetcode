99b6b3a78d376309f1be713654f28b2fbf9b06d8
WanqingYang <latejean@gmail.com>
2016-08-16 12:41:23 -0700

    [217. Contains Duplicate][Accepted]committed by LeetCode Extension

---
+++ 217-Contains-Duplicate/solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        int _size = nums.size();
+        if(_size <= 1){return false;}
+        
+        unordered_map<int, int> myMap;
+        for(int i = 0; i < _size; i++){
+            if(myMap.find(nums[i]) != myMap.end()){//contains duplicate
+                return true;
+            }
+            myMap[nums[i]] = i;
+        }
+        return false;
+    }
+};
