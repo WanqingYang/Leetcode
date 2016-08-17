fa536d12751612b2fa30a6bd18d83abeb259a019
WanqingYang <latejean@gmail.com>
2016-08-17 11:32:50 -0700

    [219. Contains Duplicate II][Accepted]committed by LeetCode Extension

---
+++ 219-Contains-Duplicate-II/solution.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        unordered_map<int, int> myMap;
+        for(int i = 0; i < nums.size(); i++){
+            if(myMap.find(nums[i]) != myMap.end() && i - myMap[nums[i]] <= k ){
+                return true;
+            }
+            myMap[nums[i]] = i;
+        }
+        return false;
+    }
+};
