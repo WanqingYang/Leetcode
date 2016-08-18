6431c92f04708635e7a9f79cc51c7de3d0065cb0
WanqingYang <latejean@gmail.com>
2016-08-17 18:53:58 -0700

    [297. Serialize and Deserialize Binary Tree][Runtime Error]committed by LeetCode Extension

--- 297-Serialize-and-Deserialize-Binary-Tree/solution.cpp
+++ 297-Serialize-and-Deserialize-Binary-Tree/solution.cpp
@@ -7,42 +7,38 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
-
-class Codec {
+class Codec{
 public:
     // Encodes a tree to a single string.
-    string serialize(TreeNode* root) {
-        ostringstream out;
-        serialize(root, out);
-        return out.str();
-    }
+    string serialize(TreeNode* root){
+      ostringstream out;
+      serialize(root, out);
+      return out.str();
+    } 
     
     // Decodes your encoded data to tree.
-    TreeNode* deserialize(string data) {
-        istringstream in(data);
+    TreeNode* deserialize(string data){
+        istringstream in;
         return deserialize(in);
     }
-
 private:
-
-    void serialize(TreeNode* root, ostringstream& out) {
-        if (root) {
-            out << root->val << ' ';
-            serialize(root->left, out);
-            serialize(root->right, out);
-        } else {
-            out << "# ";
+    void serialize(TreeNode* root, ostringstream &out){
+        if(root){
+            out << root -> val << ' ';
+            serialize(root -> left, out);
+            serialize(root -> right, out);
+        }else{
+            out << "#";
         }
     }
-
-    TreeNode* deserialize(istringstream& in) {
+    
+    TreeNode* deserialize(istringstream &in){
         string val;
         in >> val;
-        if (val == "#")
-            return nullptr;
-        TreeNode* root = new TreeNode(stoi(val));
-        root->left = deserialize(in);
-        root->right = deserialize(in);
+        if(val == "#"){return NULL;}
+        TreeNode *root = new TreeNode(stoi(val));
+        root -> left = deserialize(in);
+        root -> right = deserialize(in);
         return root;
     }
 };
