db31dbe4a08a44a88f4edaec69362cad72485227
WanqingYang <latejean@gmail.com>
2016-08-20 02:21:57 -0700

    [130. Surrounded Regions][Accepted]committed by LeetCode Extension

--- 130-Surrounded-Regions/solution.cpp
+++ 130-Surrounded-Regions/solution.cpp
@@ -9,15 +9,11 @@ public:
 	    
 	    for(i = 0; i < row; i++){
 	        check(board, i, 0, row, col);
-	        if(col > 1){ // when there are 1 or two column, every column is border
-	            check(board, i, col - 1, row, col);
-	        }
+	        check(board, i, col - 1, row, col);
 	    }
 	    for(j = 1; j + 1 < col; j++){ // from 1 to col - 1, for 0  and col - 1 already been checked
 	        check(board, 0, j, row, col);
-	        if(row > 1){// when there are 1 or two rows, every row is border
-	            check(board, row - 1, j, row, col);
-	        }
+	        check(board, row - 1, j, row, col);
 	    }
 
 	    for(i = 0; i < row; i++){
