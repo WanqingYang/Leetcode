24645b20af8658a336774e785ded880dd8fe3a37
WanqingYang <latejean@gmail.com>
2016-08-20 14:47:50 -0700

    [19. Remove Nth Node From End of List][Accepted]committed by LeetCode Extension

--- 19-Remove-Nth-Node-From-End-of-List/solution.cpp
+++ 19-Remove-Nth-Node-From-End-of-List/solution.cpp
@@ -9,12 +9,7 @@
  
 class Solution {
 public:
-    /*
-    题意：删除链表中倒数第n个结点，尽量只扫描一遍。
-    使用两个指针扫描，当第一个指针扫描到第N个结点后，
-    第二个指针从表头与第一个指针同时向后移动，
-    当第一个指针指向空节点时，另一个指针就指向倒数第n个结点了
-    */
+
     /*ListNode* removeNthFromEnd(ListNode* head, int n) {
         ListNode* pre = new ListNode(0);
         pre -> next = head;
@@ -33,6 +28,12 @@ public:
         return pre -> next;
     }*/
     
+    /*
+    题意：删除链表中倒数第n个结点，尽量只扫描一遍。
+    使用两个指针扫描，当第一个指针扫描到第N个结点后，
+    第二个指针从表头与第一个指针同时向后移动，
+    当第一个指针指向空节点时，另一个指针就指向倒数第n个结点了
+    */    
     ListNode* removeNthFromEnd(ListNode* head, int n){
         ListNode dummy(0);
         ListNode *pre = &dummy;
