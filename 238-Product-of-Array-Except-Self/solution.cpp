afa35b56652d5f34b1a2ad77d158a23014e35525
WanqingYang <latejean@gmail.com>
2016-08-29 11:43:38 -0700

    [238. Product of Array Except Self][Compile Error]committed by LeetCode Extension

--- 238-Product-of-Array-Except-Self/solution.cpp
+++ 238-Product-of-Array-Except-Self/solution.cpp
@@ -1,4 +1,9 @@
-
+/**reserve the vector of int with value 1 and size n;
+ * in the for loop from i to n - 1, update the result vector:
+ * int fromBeigin is the products from 0 to n-1;
+ * int fromLast is the producs from n-1 to 0
+ * result update 2n times, finally get the answer
+ **/
 //http://fisherlei.blogspot.com/2015/10/leetcode-product-of-array-except-self.html
 class Solution {
 public:
