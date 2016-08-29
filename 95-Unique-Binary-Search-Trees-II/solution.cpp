7a6179f4bc13d8de8b847135d006c2ef93ae32c7
WanqingYang <latejean@gmail.com>
2016-08-28 23:29:36 -0700

    [95. Unique Binary Search Trees II][Accepted]committed by LeetCode Extension

--- 95-Unique-Binary-Search-Trees-II/solution.cpp
+++ 95-Unique-Binary-Search-Trees-II/solution.cpp
@@ -17,30 +17,31 @@ public:
                return vector<TreeNode*>();
            }  
             return *generate(1, n);  
-       }  
+       }
+       
        vector<TreeNode *> *generate(int start, int end)  
        {  
-            vector<TreeNode *> *subTree = new vector<TreeNode*>();  
-            if(start>end)  
-            {  
-                 subTree->push_back(NULL);  
+            vector<TreeNode *> *subTree = new vector<TreeNode*>(); //in heap, will be there until the program ends; when loop ends, the subTree will be destroyed, but *subTree will not 
+            if(start > end)  
+            {
+                 subTree -> push_back(NULL);  
                  return subTree;  
             }  
-            for(int i =start; i<=end; i++)  
+            for(int i = start; i <= end; i++)  
             {  
-                 vector<TreeNode*> *leftSubs = generate(start, i-1);  
-                 vector<TreeNode*> *rightSubs = generate(i+1, end);  
-                 for(int j = 0; j< leftSubs->size(); j++)  
+                 vector<TreeNode*> *leftSubs = generate(start, i-1);
+                 vector<TreeNode*> *rightSubs = generate(i+1, end);
+                 for(int j = 0; j < leftSubs -> size(); j++)  
                  {  
-                      for(int k=0; k<rightSubs->size(); k++)  
+                      for(int k = 0; k < rightSubs -> size(); k++)
                       {  
-                           TreeNode *node = new TreeNode(i);  
-                           node->left = (*leftSubs)[j];  
-                           node->right = (*rightSubs)[k];  
-                           subTree->push_back(node);  
+                           TreeNode *node = new TreeNode(i);
+                           node -> left = (*leftSubs)[j];
+                           node -> right = (*rightSubs)[k];
+                           subTree -> push_back(node);
                       }  
                  }  
-            }  
+            }
             return subTree;  
        }      
 };
