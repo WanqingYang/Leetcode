77ffe8f6136092fff45c9a51f40d7e70b9c880ea
WanqingYang <latejean@gmail.com>
2016-08-30 11:56:03 -0700

    [16. 3Sum Closest][Runtime Error]committed by LeetCode Extension

---
+++ 16-3Sum-Closest/solution.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int threeSumClosest(vector<int>& nums, int target) {
+        int n = nums.size();
+        if(n < 3){return 0;}//less than 3 ele, return 3
+        int closest = nums[0] + nums[1] + nums[2];
+        sort(nums.begin(), nums.end());
+        
+        for(int first = 0; first < n; first ++){
+            if(first > 0 && nums[first] == nums[first - 1]){continue;}
+            int second = first + 1;
+            int third = n - 1;
+            
+            while(second < third){
+                int curSum = nums[first] + nums[second] + nums[third];
+                if(curSum == target){return curSum;}
+                if(abs(target - curSum) < abs(target - closest)){closest = curSum;}
+                if(curSum < target){second++;}
+                else{third++;} //curSum > target
+            }
+        }
+        return closest;
+    }
+};
