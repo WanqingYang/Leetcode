3d17dae33b6abe3e679f665d4c548d0e7c0d0686
WanqingYang <latejean@gmail.com>
2016-09-01 11:03:15 -0700

    [46. Permutations][Accepted]committed by LeetCode Extension

---
+++ 46-Permutations/solution.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> res;
+        permuteRecursion(nums, 0, res);
+        return res;
+    }
+    
+    void permuteRecursion(vector<int> &nums, int begin, vector<vector<int>> &res){
+        if(begin == nums.size()){ // ?? why need >= 
+            res.push_back(nums);
+            return;
+        }
+        
+        for(int i = begin; i < nums.size(); i++){
+            swap(nums[begin], nums[i]);
+            permuteRecursion(nums, begin + 1, res);
+            swap(nums[begin], nums[i]);
+        }
+    }
+};
