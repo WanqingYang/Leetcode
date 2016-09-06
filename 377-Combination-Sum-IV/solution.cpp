c2e66afdfd675c71007ad27f52d40c8a5623def0
WanqingYang <latejean@gmail.com>
2016-09-06 13:16:59 -0700

    [377. Combination Sum IV][Accepted]committed by LeetCode Extension

--- 377-Combination-Sum-IV/solution.cpp
+++ 377-Combination-Sum-IV/solution.cpp
@@ -1,5 +1,20 @@
-
 class Solution {
+public:
+    int combinationSum4(vector<int>& nums, int target) {
+        vector<int> dp(target + 1);
+        dp[0] = 1;
+        sort(nums.begin(), nums.end());
+        for (int i = 1; i <= target; i++) {
+            for (auto num : nums) {
+                if (i < num) break;
+                dp[i] += dp[i - num];
+            }
+        }
+        return dp.back();
+    }
+};
+
+/*class Solution {
 public:
     int combinationSum4(vector<int>& nums, int target) {
         sort(nums.begin(), nums.end());
@@ -7,11 +22,10 @@ public:
         return res.size();
     }
     
-private:
-    vector<int> path;
-    vector<vector<int>> res;
-    
-    void combineSumFinder4(vector<int>& nums, int target) {
+    int combinationSum4(vector<int>& nums, int target) {
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> res;
+        
         if(target == 0){
             res.push_back(path);
             return;
@@ -19,8 +33,14 @@ private:
         
         for(int i = 0; i < nums.size() && target >= nums[i]; i++){
             path.push_back(nums[i]);
-            combineSumFinder4(nums, target - nums[i]);
+            combinationSum4(nums, target - nums[i]);
             path.pop_back();
         }
-    }
+    }    
+    
+private:
+    vector<int> path;
+    vector<vector<int>> res;
+    
+
 };
