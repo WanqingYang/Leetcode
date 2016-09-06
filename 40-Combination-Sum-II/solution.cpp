c87a69e822fa10124e742c6f40316c6effd4e808
WanqingYang <latejean@gmail.com>
2016-09-06 12:26:08 -0700

    [40. Combination Sum II][Accepted]committed by LeetCode Extension

---
+++ 40-Combination-Sum-II/solution.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        sort(candidates.begin(), candidates.end());
+        vector<int> path;
+        vector<vector<int>> res;
+        combineSumFinder2(candidates, target, 0, path, res);
+        return res;
+    }
+
+private:
+    void combineSumFinder2(vector<int>& candidates, int target, int begin, vector<int> &path, 
+    vector<vector<int>> &res){
+        if(target == 0){
+            res.push_back(path);
+            return;
+        }
+        for(int i = begin; i < candidates.size() && target >= candidates[i]; i++){
+            if(i == begin || candidates[i] != candidates[i - 1]){  //remove duplicates candidates  
+                path.push_back(candidates[i]);
+                combineSumFinder2(candidates, target - candidates[i], i + 1, path, res);
+                path.pop_back();
+            }
+        }
+    }
+};
