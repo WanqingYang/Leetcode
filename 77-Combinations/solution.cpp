216224c2ddf5d6ea9161230c246920d465efb61d
WanqingYang <latejean@gmail.com>
2016-09-06 11:09:59 -0700

    [77. Combinations][Accepted]committed by LeetCode Extension

--- 77-Combinations/solution.cpp
+++ 77-Combinations/solution.cpp
@@ -1,5 +1,26 @@
 class Solution {
 public:
+    vector<vector<int> > combine(int n, int k) {
+        v.resize(k);
+        dfs(1, n, k);
+        return r;
+    }
+private:
+    vector<vector<int> > r;
+    vector<int> v;
+    void dfs(int i, int n, int k) {
+        while (i <= n) {
+            v[v.size() - k] = i++;
+            if (k > 1)
+                dfs(i, n, k - 1);
+            else
+                r.push_back(v);
+        }
+    }
+};
+
+/*class Solution {
+public:
     vector<vector<int>> combine(int n, int k) {
         vector<int> path;
         vector<vector<int>> res;
