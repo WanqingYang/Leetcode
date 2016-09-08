c86a61ce4c359dec7f0b4a4951a94f6f3bc67a07
WanqingYang <latejean@gmail.com>
2016-09-08 13:04:04 -0700

    [301. Remove Invalid Parentheses][Compile Error]committed by LeetCode Extension

--- 301-Remove-Invalid-Parentheses/solution.cpp
+++ 301-Remove-Invalid-Parentheses/solution.cpp
@@ -7,6 +7,62 @@ public:
 
 class Solution {
 public:
+  vector<string> removeInvalidParentheses(string s) {
+    int left = 0, right = 0;
+    for (char ch : s) {
+      if (ch == '(') {
+        ++left;
+      } else if (ch == ')') {
+        if (left > 0) {
+          --left;
+        } else {
+          ++right;
+        }
+      }
+    }
+    res.clear();
+    dfs(s, 0, left, right, 0, "");
+    return res;
+  }
+  
+private:
+  void dfs(const string& s, int start, int left, int right, 
+           int open, string path) {
+    if (start == s.length()) {
+      if (left == 0 && right == 0 && open == 0) {
+        res.push_back(path);
+      }
+      return;
+    }
+    if (left < 0 || right < 0 || open < 0) return;
+    
+    char c = s[start];
+    if (c == '(') {
+      if (start == 0 || s[start - 1] != c) {
+        for (int i = 0; start + i < s.length() && s[start + i] == c && i + 1 <= left; ++i) {
+          dfs(s, start + i + 1, left - i - 1, right, open, path);
+        }
+      }
+      
+      dfs(s, start + 1, left, right, open + 1, path + c);
+    } else if (c == ')') {
+      if (start == 0 || s[start - 1] != c) {
+        for (int i = 0; start + i < s.length() && s[start + i] == c && i + 1 <= right; ++i) {
+          dfs(s, start + i + 1, left, right - i - 1, open, path);
+        }
+      }      
+      
+      dfs(s, start + 1, left, right, open - 1, path + c);
+    } else {
+      dfs(s, start + 1, left, right, open, path + c);
+    }
+  }
+
+  vector<string> res;
+};
+
+/*class Solution {
+public:
     vector<string> removeInvalidParentheses(string s) {
         int remove_left=0, remove_right=0, pair=0;
         /*** use the unordered_set to deal with the duplicate cases ***/
