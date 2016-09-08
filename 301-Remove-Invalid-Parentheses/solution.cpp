e3007405856489c5bd20bc96d3e8f97081b5a63a
WanqingYang <latejean@gmail.com>
2016-09-08 14:21:58 -0700

    [301. Remove Invalid Parentheses][Accepted]committed by LeetCode Extension

--- 301-Remove-Invalid-Parentheses/solution.cpp
+++ 301-Remove-Invalid-Parentheses/solution.cpp
@@ -1,62 +1,52 @@
-/*class Solution {
-public:
-    vector<string> removeInvalidParentheses(string s) {
-        
-    }
-};*/
-
 class Solution {
 public:
-  vector<string> removeInvalidParentheses(string s) {
-    int left = 0, right = 0;
-    for (char ch : s) {
-      if (ch == '(') {
-        ++left;
-      } else if (ch == ')') {
-        if (left > 0) {
-          --left;
-        } else {
-          ++right;
+    vector<string> removeInvalidParentheses(string s) {
+        int left = 0, right = 0;
+        for(char ch: s){
+            if(ch == '('){
+                left++;
+            }else if(ch == ')'){
+                if(left > 0){
+                    left--;
+                }else{
+                    right++;
+                }
+            }
         }
-      }
+        dfs(s, "", 0, left, right, 0);
+        return res;
     }
-    res.clear();
-    dfs(s, 0, left, right, 0, "");
-    return res;
-  }
-  
 private:
-  void dfs(const string& s, int start, int left, int right, 
-           int open, string path) {
-    if (start == s.length()) {
-      if (left == 0 && right == 0 && open == 0) {
-        res.push_back(path);
-      }
-      return;
-    }
-    if (left < 0 || right < 0 || open < 0) return;
-    
-    char c = s[start];
-    if (c == '(') {
-      if (start == 0 || s[start - 1] != c) {
-        for (int i = 0; start + i < s.length() && s[start + i] == c && i + 1 <= left; ++i) {
-          dfs(s, start + i + 1, left - i - 1, right, open, path);
+    vector<string> res; 
+    void dfs(const string &s, string path, int start, int left, int right, int open){
+        //base case
+        if(start == s.size()){
+            if(left == 0 && right == 0 && open == 0){
+                res.push_back(path);
+            }
+            return;
         }
-      }
-      
-      dfs(s, start + 1, left, right, open + 1, path + c);
-    } else if (c == ')') {
-      if (start == 0 || s[start - 1] != c) {
-        for (int i = 0; start + i < s.length() && s[start + i] == c && i + 1 <= right; ++i) {
-          dfs(s, start + i + 1, left, right - i - 1, open, path);
+        if(left < 0 || right < 0 || open < 0){return;}
+        
+        char c = s[start];
+        if(c == '('){
+            if (start == 0 || s[start - 1] != c) {
+                //remove the '('
+                for (int i = 0; start + i < s.size() && s[start + i] == c && i + 1 <= left; ++i){
+                    dfs(s, path, start + i + 1, left - i - 1, right, open);
+                }
+            }
+            dfs(s, path + c, start + 1, left, right, open + 1);//keep ')'
+        }else if(c == ')'){
+            if (start == 0 || s[start - 1] != c) {
+                //remove the ')'
+                for (int i = 0; start + i < s.size() && s[start + i] == c && i + 1 <= right; ++i) {
+                    dfs(s, path, start + i + 1, left, right - i - 1, open);
+                }
+            }
+            dfs(s, path + c, start + 1, left, right, open - 1);
+        }else{
+            dfs(s, path + c, start + 1, left, right, open);
         }
-      }      
-      
-      dfs(s, start + 1, left, right, open - 1, path + c);
-    } else {
-      dfs(s, start + 1, left, right, open, path + c);
     }
-  }
-
-  vector<string> res;
 };
