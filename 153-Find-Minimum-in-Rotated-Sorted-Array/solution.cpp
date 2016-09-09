cd99590297b59cf4826205a1611c7d3e116087e3
WanqingYang <latejean@gmail.com>
2016-09-09 12:43:02 -0700

    [153. Find Minimum in Rotated Sorted Array][Accepted]committed by LeetCode Extension

--- 153-Find-Minimum-in-Rotated-Sorted-Array/solution.cpp
+++ 153-Find-Minimum-in-Rotated-Sorted-Array/solution.cpp
@@ -9,19 +9,20 @@ public:
         int start = 0;
         int end = nums.size() - 1;
         
-        while(start  < end){
+        while(start + 1 < end){
             /*if(nums[start] <= nums[end]){//????
                 return nums[start];
             }*/
             
             int mid = start + (end - start)/2;
             if(nums[mid] >= nums[end]){
-                start = mid + 1;//must not in the index from start to mid
+                start = mid;//must not in the index from start to mid
             }else{
                 end = mid;
             }
         }
+        return nums[start] <= nums[end]? nums[start] : nums[end];
 
-        return nums[start];
+        //return nums[start];
     }
 };
