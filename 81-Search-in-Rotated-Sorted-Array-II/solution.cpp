3dfd666a25f9e896ca5a182a29a3776adae36420
WanqingYang <latejean@gmail.com>
2016-09-09 14:13:27 -0700

    [81. Search in Rotated Sorted Array II][Accepted]committed by LeetCode Extension

--- 81-Search-in-Rotated-Sorted-Array-II/solution.cpp
+++ 81-Search-in-Rotated-Sorted-Array-II/solution.cpp
@@ -1,29 +1,33 @@
+/**similar to  Find Minimum in Rotated Sorted Array II:
+ * https://leetcode.com/problems/find-minimum-in-rotated-sorted-array-ii/
+ * and Search in Rotated Sorted Array: https://leetcode.com/problems/search-in-rotated-sorted-array/
+ **/
 class Solution {
 public:
     bool search(vector<int>& nums, int target) {
         int start = 0;
         int end = nums.size() - 1;
         
-        while(start < end){
+        while(start + 1 < end){
             int mid = start + (end - start) / 2;
             if(nums[mid] == target){return true;}
             
             if(nums[mid] > nums[end]){
                 if(target >= nums[start] && target < nums[mid]){
-                    end = mid - 1;
+                    end = mid;
                 }else{
-                    start = mid + 1;
+                    start = mid;
                 }
             }else if(nums[mid] < nums[end]){
                 if(target > nums[mid] && target <= nums[end]){
-                    start = mid + 1;
+                    start = mid;
                 }else{
-                    end = mid - 1;
+                    end = mid;
                 }
             }else{//==
                 end--;
             }
         }
-        return nums[start] == target? true: false;
+        return nums[start] == target || nums[end] == target? true: false;
     }
 };
