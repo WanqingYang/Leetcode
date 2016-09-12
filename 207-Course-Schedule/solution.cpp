8b07cc523443677f8f59d9b8bcba18bc1a2cb124
WanqingYang <latejean@gmail.com>
2016-09-11 21:09:05 -0700

    [207. Course Schedule][Accepted]committed by LeetCode Extension

--- 207-Course-Schedule/solution.cpp
+++ 207-Course-Schedule/solution.cpp
@@ -35,25 +35,32 @@ class Solution {
 public:
     bool canFinish(int numCourses, vector<pair<int, int>>& prerequisites) {
         vector<unordered_set<int>> graph = make_graph(numCourses, prerequisites);
-        vector<bool> onpath(numCourses, false), visited(numCourses, false);
-        for (int i = 0; i < numCourses; i++)
-            if (!visited[i] && dfs_cycle(graph, i, onpath, visited))
+        vector<bool> onpath(numCourses, false);
+        vector<bool> visited(numCourses, false);
+        //if visited[i] is true,means from i to all nodes which i is their prerequisites, has no cycle
+        for (int i = 0; i < numCourses; i++) {
+            if(!visited[i] && dfs_cycle(graph, i, onpath, visited)){
                 return false;
+            }
+        }
         return true;
     }
 private:
     vector<unordered_set<int>> make_graph(int numCourses, vector<pair<int, int>>& prerequisites) {
         vector<unordered_set<int>> graph(numCourses);
-        for (auto pre : prerequisites)
+        for (auto pre: prerequisites) {
             graph[pre.second].insert(pre.first);
+        }
         return graph;
-    } 
-    bool dfs_cycle(vector<unordered_set<int>>& graph, int node, vector<bool>& onpath, vector<bool>& visited) {
-        if (visited[node]) return false;
-        onpath[node] = visited[node] = true; 
-        for (int neigh : graph[node])
-            if (onpath[neigh] || dfs_cycle(graph, neigh, onpath, visited))
-                return true;
-        return onpath[node] = false;
+    }
+    
+    bool dfs_cycle(vector<unordered_set<int>> &graph, int node, vector<bool> &onpath, 
+    vector<bool> &visited) {
+        if(visited[node] == true){return false;}
+        onpath[node] = visited[node] = true;//???
+        for (int neigh: graph[node]) {
+            if(onpath[neigh] || dfs_cycle(graph, neigh, onpath, visited)) {return true;}
+        }
+        return onpath[node] = false;// backtracking mark onpath[node] is false and return false
     }
 };
