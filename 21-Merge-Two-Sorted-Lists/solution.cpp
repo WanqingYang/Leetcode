7e40259d447fe5501afdb6bad739922f30f8d188
WanqingYang <latejean@gmail.com>
2016-09-20 11:32:24 -0700

    [21. Merge Two Sorted Lists][Accepted]committed by LeetCode Extension

--- 21-Merge-Two-Sorted-Lists/solution.cpp
+++ 21-Merge-Two-Sorted-Lists/solution.cpp
@@ -7,10 +7,43 @@
  * };
  */
 class Solution {
+public:    
+    ListNode *mergeTwoLists(ListNode *l1, ListNode *l2) {
+        ListNode dummy(0);
+        ListNode* result = &dummy;
+        //merge_helper(l1, l2, result);
+        while(l1 && l2) {
+            if(l1->val <= l2->val) {
+                result->next = l1;
+                l1 = l1->next;
+            }else {
+                result->next = l2;
+                l2 = l2->next;
+            }
+            result = result->next;
+        }
+        /*while(l1) {
+            result->next = l1;
+            l1 = l1->next;
+        }
+        while(l2) {
+            result->next = l2;
+            l2 = l2->next;
+        }*/
+        result->next = l1? l1 :l2;
+        
+        return dummy.next;
+    }
+};
+ 
+/*class Solution {
 public:
     void merge_helper(ListNode *l1, ListNode *l2, ListNode *tail){
     //ListNode* merge_helper(ListNode *l1, ListNode *l2, ListNode *tail){
-        if(l1 == NULL || l2 == NULL){tail->next = l1?l1:l2; return;}
+        if(l1 == NULL || l2 == NULL){
+            tail->next = l1?l1:l2; 
+            return;
+        }
         if(l1->val <= l2->val){
             tail->next = l1;
             l1 = l1->next;
@@ -26,7 +59,7 @@ public:
         ListNode dummy_head(0);
         ListNode *tail = &dummy_head;
         merge_helper(l1, l2, tail);
-        //tail -> next = l1? l1 : l2;  //
+        //tail -> next = l1? l1 : l2;  //?????
         return dummy_head.next;
         
      /*   //ListNode* pre = new ListNode(0);
@@ -47,6 +80,6 @@ public:
         tail -> next = l1? l1 : l2;
         //ListNode* pre1 = pre->next;
         //delete pre; 
-        return pre->next;  */
+        return pre->next;
     }
 };
