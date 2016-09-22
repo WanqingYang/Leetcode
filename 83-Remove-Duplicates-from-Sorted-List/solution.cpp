59d702742c5f0f9694dcf9e939838b574feb71e0
WanqingYang <latejean@gmail.com>
2016-09-22 12:43:20 -0700

    [83. Remove Duplicates from Sorted List][Accepted]committed by LeetCode Extension

--- 83-Remove-Duplicates-from-Sorted-List/solution.cpp
+++ 83-Remove-Duplicates-from-Sorted-List/solution.cpp
@@ -6,14 +6,37 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
+//second time, rewrite
 class Solution {
 public:
     ListNode* deleteDuplicates(ListNode* head) {
+        if(head == NULL || head->next == NULL) {
+            return head;
+        }
+        
+        for(ListNode *pre = head, *cur = pre->next; cur != NULL; cur = cur->next) {
+            if(pre->val == cur->val) {
+                pre->next = cur->next;
+                delete cur;
+                cur = pre;
+            } else {
+                pre = cur;
+            }
+        }
+        return head;
+    }
+};
+
+/*class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
         if(head == NULL){return head;}
+        //below no need for repeating intialization
         for(ListNode* pre = head, *cur = pre->next; cur != NULL; cur = cur->next){
             if(pre->val == cur->val){
                 pre->next = cur->next;
                 delete cur;
+                cur = pre;
             } else{
                 pre = cur;
             }
