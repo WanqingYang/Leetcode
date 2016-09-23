1679756d0eaeb79c16dfcb2efe0aa0aba2e84b96
WanqingYang <latejean@gmail.com>
2016-09-23 13:26:17 -0700

    [23. Merge k Sorted Lists][Time Limit Exceeded]committed by LeetCode Extension

--- 23-Merge-k-Sorted-Lists/solution.cpp
+++ 23-Merge-k-Sorted-Lists/solution.cpp
@@ -6,13 +6,44 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
+//priority_queue
 class Solution {
 public:
     /**
      * @param lists: a list of ListNode
      * @return: The head of one sorted list.
      */
-    ListNode *mergeTwoLists(ListNode* l1, ListNode* l2) {
+    ListNode *mergeKLists(vector<ListNode *> &lists) {
+        if(lists.empty()) {return NULL;}
+        
+        priority_queue<ListNode *> pq;
+        for(int i = 0; i < lists.size(); i++) {
+            if(lists[i] != NULL) {
+                pq.push(lists[i]);
+            }
+        }
+        
+        ListNode dummy(0);
+        ListNode* cur = &dummy;
+        while(!pq.empty()) {
+            ListNode *head = pq.top();
+            cur->next = head;
+            cur = head;
+            if(head->next != NULL) {
+                pq.push(head->next);
+            }
+        }
+        return dummy.next;
+    }
+};
+ 
+/*class Solution {
+public:
+    /**
+     * @param lists: a list of ListNode
+     * @return: The head of one sorted list.
+     */
+    /*ListNode *mergeTwoLists(ListNode* l1, ListNode* l2) {
         if (NULL == l1) return l2;
         else if (NULL == l2) return l1;
         if (l1->val <= l2->val) {
