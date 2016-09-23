d70550c48279645b2cdfd566a0ed53cbff08635d
WanqingYang <latejean@gmail.com>
2016-09-23 11:03:12 -0700

    [352. Data Stream as Disjoint Intervals][Accepted]committed by LeetCode Extension

--- 352-Data-Stream-as-Disjoint-Intervals/solution.cpp
+++ 352-Data-Stream-as-Disjoint-Intervals/solution.cpp
@@ -7,59 +7,56 @@
  *     Interval(int s, int e) : start(s), end(e) {}
  * };
  */
-/*class SummaryRanges {
-public:
-    /** Initialize your data structure here. **/
-    /*SummaryRanges() {
-        
-    }
-    
-    void addNum(int val) {
-        
-    }
-    
-    vector<Interval> getIntervals() {
-        
-    }
-};*/
-
-/*class SummaryRanges {
-public:
-    void addNum(int val) {
-        auto Cmp = [](Interval a, Interval b) { return a.start < b.start; };
-        auto it = lower_bound(vec.begin(), vec.end(), Interval(val, val), Cmp);
-        int start = val, end = val;
-        if(it != vec.begin() && (it-1)->end+1 >= val) it--;
-        while(it != vec.end() && val+1 >= it->start && val-1 <= it->end)
-        {
-            start = min(start, it->start);
-            end = max(end, it->end);
-            it = vec.erase(it);
-        }
-        vec.insert(it,Interval(start, end));
-    }
-    
-    vector<Interval> getIntervals() {
-        return vec;
-    }
-private:
-    vector<Interval> vec;
-};*/
-
+ //first time write by myself
 class SummaryRanges {
 public:
-    /** Initialize your data structure here. */
+    /** Initialize your data structure here. **/
+    SummaryRanges() {
+        
+    }
+    
     void addNum(int val) {
-        auto it = st.lower_bound(Interval(val, val)); //know where the val's index is, in the st
+        auto it = st.lower_bound(Interval(val, val));
         int start = val, end = val;
-        if(it != st.begin() && (--it)->end+1 < val) {it++;} //it is neither the first nor???????
-        while(it != st.end() && val+1 >= it->start && val-1 <= it->end)// not the last, and val is in the
-        {
+        //check which interval the val should be in
+        if(it != st.begin() && (--it)->end + 1 < val) {it++;}
+        while(it != st.end() && val >= it->start - 1 && val <= it->end + 1) {
             start = min(start, it->start);
             end = max(end, it->end);
             it = st.erase(it);
         }
-        st.insert(it,Interval(start, end));
+        st.insert(it, Interval(start, end));
+    }
+    
+    vector<Interval> getIntervals() {
+        vector<Interval> result;
+        for(val: st) {result.push_back(val);}
+        return result;
+    }
+private:
+    struct cmp {
+        bool operator()(Interval a, Interval b) {return a.start < b.start;}
+    };
+    set<Interval, cmp> st;
+
+};
+
+/*class SummaryRanges {
+public:
+    /** Initialize your data structure here. */
+   /* void addNum(int val) {
+        auto it = st.lower_bound(Interval(val, val)); //know where the val's index is, in the st
+        int start = val, end = val;
+        if(it != st.begin() && (--it)->end+1 < val) {it++;} //it is neither the first nor the next
+        while(it != st.end() && val+1 >= it->start && val-1 <= it->end)
+        //not the last, and val is in the
+        {
+            start = min(start, it->start);
+             end = max(end, it->end);
+            it = st.erase(it); //return an iterator to the element that follows the last element //removed (or set::end, if the last element was removed).
+
+        }
+        st.insert(it,Interval(start, end));//insert the interval into position before it; 
     }
     
     vector<Interval> getIntervals() {
