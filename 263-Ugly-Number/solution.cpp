71f363f7aa03a768734abe9009611fdaa38b8cce
WanqingYang <latejean@gmail.com>
2016-09-23 14:11:41 -0700

    [263. Ugly Number][Accepted]committed by LeetCode Extension

--- 263-Ugly-Number/solution.cpp
+++ 263-Ugly-Number/solution.cpp
@@ -2,7 +2,7 @@
 class Solution {
 public:
     bool isUgly(int num) {
-        for(int i = 2; i < 6; i++) {
+        for(int i = 2; i < 6 && num; i++) {
             while(num % i == 0) {
                 num /= i;
             }
