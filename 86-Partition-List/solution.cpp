7092fa17c12cf4891119bad8d6c08fb6f2e2fe85
WanqingYang <latejean@gmail.com>
2016-09-23 11:56:18 -0700

    [86. Partition List][Accepted]committed by LeetCode Extension

--- 86-Partition-List/solution.cpp
+++ 86-Partition-List/solution.cpp
@@ -6,7 +6,31 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
+ //second time, rewrite
 class Solution {
+public:
+    ListNode* partition(ListNode* head, int x) {
+        if(head == NULL || head->next == NULL) {return head;}
+        ListNode small_dh(0), large_dh(0);
+        ListNode *small = &small_dh, *large = &large_dh;
+        
+        while(head) {
+            if(head->val < x) {
+                small->next = head;
+                small = small->next;
+            } else {
+                large->next = head;
+                large = large->next;
+            }
+            head = head->next;
+        }
+        small->next = large_dh.next;
+        large->next = NULL;
+        return small_dh.next;
+    } 
+};
+ 
+/*class Solution {
 public:
     ListNode* partition(ListNode* head, int x) {
         if(head == NULL || head -> next == NULL){return head;}
