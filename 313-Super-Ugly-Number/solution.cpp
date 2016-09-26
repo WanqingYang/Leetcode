eacdaac3ef6a08b13963d6f98d77529f388cb48b
WanqingYang <latejean@gmail.com>
2016-09-26 11:36:18 -0700

    [313. Super Ugly Number][Compile Error]committed by LeetCode Extension

--- 313-Super-Ugly-Number/solution.cpp
+++ 313-Super-Ugly-Number/solution.cpp
@@ -1,6 +1,32 @@
 class Solution {
 public:
     int nthSuperUglyNumber(int n, vector<int>& primes) {
+        int _size = primes.size();
+        vector<int> res(1,1);
+        vector<int> cnt(_size, 0);
+        vector<int> val(_size, 1);
+        
+        int minimum = 1;
+        //while(res.size() < n) {
+        for(int i = 0; i < _size; i++) {
+            res[i] = minimum;
+            
+            minimum = INT_MAX;
+            for(int j = 0; j < _size; j++) {
+                //skip duplicate and avoid extra multiplication
+                if(val[j] == res[i]) {
+                    val[j] = res[cnt[j]++] * primes[j];
+                }
+                minimum = min(minimum, val[j]);
+            }
+        }
+        return res[n-1];
+    }
+};
+
+/*class Solution {
+public:
+    int nthSuperUglyNumber(int n, vector<int>& primes) {
         vector<int> res(1,1);
         int _size = primes.size();
         vector<int> cnt(_size, 0);
