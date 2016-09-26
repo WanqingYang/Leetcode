d04032a83a823e4dfa5cdeb284aff92a34204014
WanqingYang <latejean@gmail.com>
2016-09-25 17:43:54 -0700

    [125. Valid Palindrome][Accepted]committed by LeetCode Extension

--- 125-Valid-Palindrome/solution.cpp
+++ 125-Valid-Palindrome/solution.cpp
@@ -1,6 +1,17 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
+        for(int i = 0, j = s.size() - 1; i < j; i++, j--) {
+            while(i < j && !isalnum(s[i])) {i++;}
+            while(j > i && !isalnum(s[j])) {j--;}
+            if(tolower(s[i]) != tolower(s[j])) {return false;}
+        }
+        return true;
+    }
+};
+/*class Solution {
+public:
+    bool isPalindrome(string s) {
         if(s.empty()) {return true;}
         int i = 0, j = s.size() - 1;
         while(i < j) {
