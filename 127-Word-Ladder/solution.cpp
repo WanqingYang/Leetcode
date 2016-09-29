86920016a8291abc023e6295a13c54a66747b123
WanqingYang <latejean@gmail.com>
2016-09-29 12:27:39 -0700

    [127. Word Ladder][Compile Error]committed by LeetCode Extension

--- 127-Word-Ladder/solution.cpp
+++ 127-Word-Ladder/solution.cpp
@@ -6,6 +6,41 @@ To simplify the problem, we insert end into dict. Once we meet end during the BF
 class Solution {
 public:
     int ladderLength(string beginWord, string endWord, unordered_set<string>& wordDict) {
+        wordDict.insert(endWord);
+        queue<string> toVisit;
+        addNextWords(beginWord, wordDict, toVisit);
+        int dist = 2;
+        while (!toVisit.empty()) {
+            int num = toVisit.size();
+            for (int i = 0; i < num; i++) {
+                string word = toVisit.front();
+                toVisit.pop();
+                if (word == endWord) return dist;
+                addNextWords(word, wordDict, toVisit);
+            }
+            dist++;
+        }
+    }
+private:
+    void addNextWords(string word, unordered_set<string>& wordDict, queue<string>& toVisit) {
+        wordDict.erase(word);
+        for (int p = 0; p < (int)word.length(); p++) {
+            char letter = word[p];
+            for (int k = 0; k < 26; k++) { 
+                word[p] = 'a' + k;
+                if (wordDict.find(word) != wordDict.end()) {
+                    toVisit.push(word);
+                    wordDict.erase(word);
+                }
+            }
+            word[p] = letter;
+        } 
+    } 
+};
+
+/*class Solution {
+public:
+    int ladderLength(string beginWord, string endWord, unordered_set<string>& wordDict) {
         //if(beginWord == endWord)??
         wordDict.insert(endWord); //dont forget!!!
         int ladder = 2;
@@ -23,7 +58,7 @@ public:
             //}
         }*/
         
-        while(!toVisit.empty()) {
+       /* while(!toVisit.empty()) {
             int _size = toVisit.size();
             for(int i = 0; i < _size; i++) {
                 string word = toVisit.front();
