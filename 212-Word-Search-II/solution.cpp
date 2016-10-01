7468cc3d3ba37076f37adfe9327dcc420955c516
WanqingYang <latejean@gmail.com>
2016-10-01 14:56:26 -0700

    [212. Word Search II][Compile Error]committed by LeetCode Extension

--- 212-Word-Search-II/solution.cpp
+++ 212-Word-Search-II/solution.cpp
@@ -54,7 +54,8 @@ private:
             res.push_back(words[root->wordIdx]);
             root->isLeaf = false;
             validCnt++;
-            //return validCnt; //??why not return directly?????
+            root->prefixCnt -= validCnt
+            return validCnt; //??why not return directly?????
         }
         
         //int curC = board[i][j] - 'a';
