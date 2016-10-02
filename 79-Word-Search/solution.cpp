c00df37fe11a8487857b086d370b6b05a8f60edd
WanqingYang <latejean@gmail.com>
2016-10-01 23:59:14 -0700

    [79. Word Search][Accepted]committed by LeetCode Extension

--- 79-Word-Search/solution.cpp
+++ 79-Word-Search/solution.cpp
@@ -1,26 +1,30 @@
 class Solution {
 private:
-    int row, col, _size;
+    //int row, col;
     bool dfs(vector<vector<char>>& board, string &word, int idx, int i, int j) {
-        if(idx == _size) {return true;}
-        if(i < 0 || i >= row || j < 0 || j >= col || board[i][j] != word[idx] || board[i][j] == '*'){
+        //if(idx == _size) {return true;}
+        //cout<<"entering dfs, idx, i, j = "<< idx << i << j << endl;
+        if(i < 0 || i >= board.size() || j < 0 || j >= board[0].size() /*|| idx >= word.size()*/ || board[i][j] != word[idx] || board[i][j] == '*'){
+            //cout << "got here! directly return false, j = " << j << endl;
             return false;
         }
-        //if(idx == _size) {return true;}
+        if(idx == word.size() - 1) {return true;}
         char origin = board[i][j];
         board[i][j] = '*';
         if(dfs(board, word, idx+1, i-1, j) || dfs(board, word, idx+1, i+1, j) || dfs(board, word, idx+1, i, j-1) || 
         dfs(board, word, idx+1, i, j+1)) {return true;}
-        board[j][j] = origin;
+        //board[j][j] = origin;
+        board[i][j] = origin;
         return false;
     }
     
 public:
     bool exist(vector<vector<char>>& board, string word) {
-        _size = word.size();
+        int row, col, _size = word.size();
         if(!(row = board.size()) || !(col = board[0].size()) || !_size) {return false;}
         for(int i = 0; i < row; i++) {
             for(int j = 0; j < col; j++) {
+                //cout << "board[i][j] = " << board[i][j] << endl;
                 if(dfs(board, word, 0, i, j)){return true;}
             }
         }
