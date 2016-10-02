2a90a81aa956e5feef72057622e229fa92d66622
WanqingYang <latejean@gmail.com>
2016-10-02 00:56:35 -0700

    [189. Rotate Array][Accepted]committed by LeetCode Extension

--- 189-Rotate-Array/solution.cpp
+++ 189-Rotate-Array/solution.cpp
@@ -2,9 +2,10 @@ class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
         int n = nums.size();
+        k %= n; //cause k may be greater than n!!!!!
         //reverse in a range [first,last), 左闭右开
         reverse(nums.begin(), nums.begin() + n - k);
-        reverse(nums.end() - k, nums.end());
+        reverse(nums.begin() + n - k, nums.end());
         reverse(nums.begin(), nums.end());
     }
 };
