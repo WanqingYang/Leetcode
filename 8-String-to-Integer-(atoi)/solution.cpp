64af1538e452209144ea6b45b87721e5f2ed1dd0
WanqingYang <latejean@gmail.com>
2016-10-04 09:55:04 -0700

    [8. String to Integer (atoi)][Accepted]committed by LeetCode Extension

--- 8-String-to-Integer-(atoi)/solution.cpp
+++ 8-String-to-Integer-(atoi)/solution.cpp
@@ -1,4 +1,43 @@
+//how about this solution??
 class Solution {
+public:
+    int myAtoi(string str) {
+        int sign = 1, i = 0;
+        long num = 0;
+        while(str[i] == ' ') {i++;}
+        if(str[i] == '-' || str[i] == '+') {
+            sign = 1 - 2 *(str[i++] == '-');
+        }
+        
+        while(isdigit(str[i])) {
+            num = num * 10 + (str[i] -'0');
+            if(num > INT_MAX || num - 1 >= INT_MAX) {
+                if(sign == 1) {return INT_MAX;}
+                if(sign == -1) {return INT_MIN;}
+            }
+            i++;
+        }
+        return sign *num;
+    }
+};
+
+/*int atoi(const char *str) {
+    int sign = 1, base = 0, i = 0;
+    while (str[i] == ' ') { i++; }
+    if (str[i] == '-' || str[i] == '+') {
+        sign = 1 - 2 * (str[i++] == '-'); 
+    }
+    while (str[i] >= '0' && str[i] <= '9') {
+        if (base >  INT_MAX / 10 || (base == INT_MAX / 10 && str[i] - '0' > 7)) {
+            if (sign == 1) return INT_MAX;
+            else return INT_MIN;
+        }
+        base  = 10 * base + (str[i++] - '0');
+    }
+    return base * sign;
+}
+
+/*class Solution {
 public:
     int myAtoi(string str) {
         int n = str.size();
