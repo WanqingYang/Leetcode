bc7e31650021d8e6744e6a289e11987543777356
WanqingYang <latejean@gmail.com>
2016-10-04 11:33:16 -0700

    [65. Valid Number][Accepted]committed by LeetCode Extension

--- 65-Valid-Number/solution.cpp
+++ 65-Valid-Number/solution.cpp
@@ -16,32 +16,41 @@ public:
         int start = i, ecnt = 0, dcnt = 0;
         for(; i < n; i++) {
             if(str[i] == 'e') {
-                if(ecnt > 0 || i == start || i == n-1 || !number) {
+                if(ecnt > 0 || i == start || i == n-1 || number == false) {
                     return false;
                 }
                 sci = true;
+                afterE = false;
+                number = false;
                 ecnt++;
                 continue;
             }
             if(str[i] == '.') {
-                if(dcnt > 0 || sci || (i > start && str[i-1] == ' ')) {return false;}
+                if(dcnt || sci || (i > start && str[i-1] == ' ')) {return false;}
                 dcnt++;
                 continue;
             }
             if(isdigit(str[i])) {
                 if(space) {return false;}
                 number = true;
-                //afterE = true;
+                afterE = true;
                 continue;
             } else {
                 if(str[i] == ' ') {
                     if(ecnt && afterE) {return false;}
                     space = true;
                     continue;
+                } else if(str[i] == '+' || str[i] == '-') {
+                    if(ecnt && (i > start && str[i-1] == 'e')) {
+                        afterE = false;
+                        continue;
+                    }
+                } else {
+                    return false;
                 }
-                return false;
             }
         }
-        return number;
+        return number && afterE;
+        //return number;
     }
 };
