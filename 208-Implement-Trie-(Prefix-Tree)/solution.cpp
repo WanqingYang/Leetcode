d369682d331ac17935efc8389727553c48722c50
WanqingYang <latejean@gmail.com>
2016-10-04 14:08:05 -0700

    [208. Implement Trie (Prefix Tree)][Accepted]committed by LeetCode Extension

--- 208-Implement-Trie-(Prefix-Tree)/solution.cpp
+++ 208-Implement-Trie-(Prefix-Tree)/solution.cpp
@@ -7,7 +7,7 @@ public:
     TrieNode() {
         //prefixCnt = 0;
         isLeaf = false;
-        next;
+        fill_n(next, 26, nullptr);
     }
 };
 
@@ -19,17 +19,6 @@ public:
 
     // Inserts a word into the trie.
     void insert(string word) {
-        /*if(search(word)) {return;}
-        int i = 0;
-        while(startsWith(word.substr(0,i+1))) {
-            i++;
-        }
-        for(; i < word.size(); i++) {
-            int idx = word[i] - 'a';
-            root->next[idx] = new TrieNode();
-            root = root->next[idx];
-        }
-        root->isLeaf = true;*/
         TrieNode *cur = root;
         for(int i = 0; i < word.size(); i++) {
             int idx = word[i] - 'a';
@@ -44,14 +33,6 @@ public:
 
     // Returns if the word is in the trie.
     bool search(string word) {
-       /* for(int i = 0; i < word.size(); i++) {
-            int idx = word[i] - 'a';
-            if(!root->next[idx]) {//null, not exist
-                return false;
-            }
-            root = root->next[idx];
-        }
-        return root->isLeaf;*/
         TrieNode* p = find(word);
         return p != NULL && p->isLeaf;
     }
@@ -59,14 +40,6 @@ public:
     // Returns if there is any word in the trie
     // that starts with the given prefix.
     bool startsWith(string prefix) {
-        /*for(int i = 0; i < prefix.size(); i++) {
-            int idx = prefix[i] - 'a';
-            if(!root->next[idx]) {//null, not exist
-                return false;
-            }
-            root = root->next[idx];
-        }
-        return true;  */
         return find(prefix) != NULL;
     }
 
