545f96af6dfb830dcf3703f30b27a1224e45dec6
WanqingYang <latejean@gmail.com>
2016-10-06 16:40:51 -0700

    [133. Clone Graph][Runtime Error]committed by LeetCode Extension

--- 133-Clone-Graph/solution.cpp
+++ 133-Clone-Graph/solution.cpp
@@ -6,9 +6,32 @@
  *     UndirectedGraphNode(int x) : label(x) {};
  * };
  */
-
 //second time
 class Solution {
+    unordered_map<int, UndirectedGraphNode*> myMap;
+public:
+    UndirectedGraphNode *cloneGraph(UndirectedGraphNode *node) {
+        if(myMap.find(node->label) != myMap.end()) {
+            return node;
+        }
+        
+        UndirectedGraphNode *new_node = new UndirectedGraphNode(node->label);
+        int _size = node->neighbors.size();
+        new_node->neighbors.resize(_size);
+        myMap[new_node->label] = new_node;
+        
+        for(int i = 0; i < _size; i++) {
+            new_node->neighbors[i] = cloneGraph(node->neighbors[i]);
+        }
+        return new_node;
+    }
+};
+//second time
+ /**Create a new node if the node map doesn't already contain a node with this label, 
+  * otherwise return the pointer to the node with that label from the node map. 
+  * Add the children to new nodes recursively through BFS.
+  **/
+/*class Solution {
     unordered_map<int, UndirectedGraphNode*> track;
 public:
     UndirectedGraphNode *cloneGraph(UndirectedGraphNode *node) {
@@ -16,8 +39,9 @@ public:
         if(track.find(node->label) != track.end()) {
             return track[node->label];
         }
+        
         UndirectedGraphNode* new_node = new UndirectedGraphNode(node->label);
-        new_node->neighbors.resize(node->neighbors.size());
+        new_node->neighbors.resize(node->neighbors.size());//resize is better than keep push_back
         track[new_node->label] = new_node;
         
         for(int i = 0; i < new_node->neighbors.size(); i++) {
@@ -28,10 +52,6 @@ public:
     }
 };
 
- /**Create a new node if the node map doesn't already contain a node with this label, 
-  * otherwise return the pointer to the node with that label from the node map. 
-  * Add the children to new nodes recursively through BFS.
-  **/
 /*class Solution {
 private: unordered_map<UndirectedGraphNode*, UndirectedGraphNode*> myMap;
 public:
