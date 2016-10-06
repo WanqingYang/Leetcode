c118bd12c54b7d9f58f580be63941c5eec32276d
WanqingYang <latejean@gmail.com>
2016-10-06 13:33:58 -0700

    [55. Jump Game][Accepted]committed by LeetCode Extension

---
+++ 55-Jump-Game/solution.cpp
@@ -0,0 +1,19 @@
+//dp
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int n = nums.size();
+        vector<bool> dp(n, false);
+        dp[0] = true;
+        
+        for(int i = 1; i < n; i++) {
+            for(int j = i-1; j >= 0; j--) {
+                if(dp[j] == true && nums[j] >= i-j) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        return dp[n-1];
+    }
+};
