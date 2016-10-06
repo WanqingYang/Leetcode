dd1b7e10a5f4430aaa4102a1d1829cacd8b4edf5
WanqingYang <latejean@gmail.com>
2016-10-06 12:47:53 -0700

    [72. Edit Distance][Accepted]committed by LeetCode Extension

---
+++ 72-Edit-Distance/solution.cpp
@@ -0,0 +1,37 @@
+/**
+ * Replace word1[i - 1] by word2[j - 1] (dp[i][j] = dp[i - 1][j - 1] + 1 (for replacement));
+ * Delete word1[i - 1] and word1[0..i - 2] = word2[0..j - 1] (dp[i][j] = dp[i - 1][j] + 1 (for deletion));
+ * Insert word2[j - 1] to word1[0..i - 1] and word1[0..i - 1] + word2[j - 1] = word2[0..j - 1] (dp[i][j] = dp[i][j - 1] + 1 (for insertion)).
+ * 
+ * dp[i][0] = i;
+ * dp[0][j] = j;
+ * dp[i][j] = dp[i - 1][j - 1], if word1[i - 1] = word2[j - 1];
+ * dp[i][j] = min(dp[i - 1][j - 1] + 1, dp[i - 1][j] + 1, dp[i][j - 1] + 1), otherwise.
+ **/
+
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        int m = word1.size();
+        int n = word2.size();
+        //notice that, in dp[i][j], we compare the [i-1]or[j-1], cause 
+        vector<vector<int>> dp(m+1, vector<int>(n+1, 0));
+        for(int i = 1; i <= m; i++) {
+            dp[i][0] = i;
+        }
+        for(int j = 1; j <= n; j++) {
+            dp[0][j] = j;
+        }
+        
+        for(int i = 1; i <= m; i++) {
+            for(int j = 1; j <= n; j++) {
+                if(word1[i-1] == word2[j-1]) {
+                    dp[i][j] = dp[i-1][j-1];
+                } else {
+                    dp[i][j] = min(dp[i-1][j-1] + 1, min(dp[i-1][j] + 1, dp[i][j-1] +1));
+                }
+            }
+        }
+        return dp[m][n];
+    }
+};
