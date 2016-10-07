2459609d9ea4ed102d0ab2862d698c146ed5de4f
WanqingYang <latejean@gmail.com>
2016-10-07 13:57:04 -0700

    [132. Palindrome Partitioning II][Time Limit Exceeded]committed by LeetCode Extension

--- 132-Palindrome-Partitioning-II/solution.cpp
+++ 132-Palindrome-Partitioning-II/solution.cpp
@@ -13,6 +13,45 @@ class Solution {
             }
         }
         return booldp;
+    }
+    
+public:
+    int minCut(string s) {
+        int n = s.size();
+        //preparation
+        vector<vector<bool>> booldp = isPalindrome(s);
+        
+        //initialization
+        vector<int> cutdp(n+1);
+        cutdp[0] = -1;
+
+        for(int i = 1; i <= n; i++) {
+            cutdp[i] = i-1;
+            for(int j = 0; j < i; j++) {
+                if(booldp[j][i-1]) {
+                    cutdp[i] = min(cutdp[i], cutdp[j] + 1);
+                }
+            }
+        }
+        return cutdp[n];
+    }
+};
+
+/*class Solution {
+    vector<vector<bool>> isPalindrome(string s) {
+        int n = s.size();
+        vector<vector<bool>> booldp(n, vector<bool>(n, true));
+        for(int i = 0; i < n; i++) {
+            for(int j = i+1; j < n; j++) {
+                for(int a = i, b = j; b > a; a++, b--) {
+                    if(s[a] != s[b]) {
+                        booldp[i][j] = false;//notbool dp[a][b]!!!
+                        break;
+                    }
+                }
+            }
+        }
+        return booldp;
     };
     
 public:
