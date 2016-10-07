18523f83dc756c0bd63b2d8818272a90ea775b94
WanqingYang <latejean@gmail.com>
2016-10-07 14:00:29 -0700

    [132. Palindrome Partitioning II][Accepted]committed by LeetCode Extension

--- 132-Palindrome-Partitioning-II/solution.cpp
+++ 132-Palindrome-Partitioning-II/solution.cpp
@@ -5,14 +5,16 @@ public:
         //preparation
         vector<vector<bool>> booldp(n, vector<bool>(n, true));
         
-        for(int i = 0; i < n; i++) {
-            for(int j = i+1; j < n; j++) {
-                for(int a = i, b = j; b > a; a++, b--) {
-                    if(s[a] != s[b]) {
-                        booldp[i][j] = false;//notbool dp[a][b]!!!
-                        break;
-                    }
-                }
+        for (int i = 0; i < n; i++) {
+            booldp[i][i] = true;
+            if (i + 1 < n) {
+                booldp[i][i + 1] = (s[i] == s[i + 1]);
+            }
+        }
+        
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = i + 2; j < n; j++) {
+                booldp[i][j] = booldp[i + 1][j - 1] && (s[i] == s[j]);
             }
         }
         
