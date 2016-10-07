a107ce2b2ec37c1b74a0b9ad7849831aa614c817
WanqingYang <latejean@gmail.com>
2016-10-07 11:36:58 -0700

    [45. Jump Game II][Runtime Error]committed by LeetCode Extension

---
+++ 45-Jump-Game-II/solution.cpp
@@ -0,0 +1,26 @@
+//greedy
+/*class Solution {
+public:
+    int jump(vector<int>& nums) {
+        
+    }
+};*/
+
+//dp
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> dp(n, INT_MAX);
+        dp[0] = 0;
+        for(int i = 1; i <= nums[0]; i++) {
+            dp[i] = 1;
+        }
+        for(int i = 1; i < n; i++) {
+            for(int j = 1; i+j < n && j <= nums[i]; j++) {
+                dp[i+j] = min(dp[i+j], dp[i] + 1);
+            }
+        }
+        return dp[n-1];
+    }
+};
