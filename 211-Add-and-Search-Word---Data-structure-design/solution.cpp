e1a05a1e14c082126b244bc8ae178412559d0e14
WanqingYang <latejean@gmail.com>
2016-10-11 12:11:28 -0700

    [211. Add and Search Word - Data structure design][Compile Error]committed by LeetCode Extension

---
+++ 211-Add-and-Search-Word---Data-structure-design/solution.cpp
@@ -0,0 +1,60 @@
+class TrieNode {
+public:
+    bool isLeaf;
+    TrieNode *next[26];
+    TrieNode() {
+        isLeaf = false;
+        fill_n(next, 26, nullptr);
+    }
+};
+
+class WordDictionary {
+public:
+    WordDictionary() {
+        root = new TrieNode();
+    }
+
+    // Adds a word into the data structure.
+    void addWord(string word) {
+        TrieNode *cur = root;
+        for(int i = 0; i < word.size(); i++) {
+            int idx = word[i] - 'a';
+            if(!cur->next[idx]) {
+                cur->next[idx] = new TrieNode(); 
+            }
+            cur = cur->next[idx];
+        }
+        cur->isLeaf = true;
+        return;
+    }
+
+    // Returns if the word is in the data structure. A word could
+    // contain the dot character '.' to represent any one letter.
+    bool search(string word) {
+        return find(root, word.c_str());
+    }
+    
+private:
+    TrieNode *root;
+    
+    bool find(TrieNode *root, const char* word) {
+        TrieNode *cur = root;
+        for(int i = 0; word[i] && cur; i++) {
+            if(word[i] != '.') {
+                int idx = word[i] - 'a';
+                cur = cur->next[idx];
+            } else if(cur && word[i] == '.') {
+                TrieNode *tmp = cur;
+                for(int i = 0; i < 26; i++) {
+                    cur = tmp->next[i];
+                    find(cur, word + i + 1);
+                }
+        }
+       return cur && cur->isLeaf; 
+    }
+};
+
+// Your WordDictionary object will be instantiated and called as such:
+// WordDictionary wordDictionary;
+// wordDictionary.addWord("word");
+// wordDictionary.search("pattern");
