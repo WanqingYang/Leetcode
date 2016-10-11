73011efb26b391c35824f657f112d161bbd068ec
WanqingYang <latejean@gmail.com>
2016-10-11 13:43:14 -0700

    [251. Flatten 2D Vector][Accepted]committed by LeetCode Extension

--- 251-Flatten-2D-Vector/solution.cpp
+++ 251-Flatten-2D-Vector/solution.cpp
@@ -1,4 +1,29 @@
+//using iterator
 class Vector2D {
+    vector<vector<int>> :: iterator i, iEnd;
+    int j = 0;
+public:
+    Vector2D(vector<vector<int>>& vec2d) {
+        i = vec2d.begin();
+        iEnd = vec2d.end();
+    }
+    
+    int next() {
+        return (*i)[j++];
+    }
+    
+    bool hasNext() {
+        while(i != iEnd && j == (*i).size()) {
+            i++, j = 0;
+        }
+        return i != iEnd;
+    }
+    
+};
+
+//my right but slow code
+
+/*class Vector2D {
 public:
     Vector2D(vector<vector<int>>& vec2d) {
         row = vec2d.size();
