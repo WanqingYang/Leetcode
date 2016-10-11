1ef3418bc9bbdeee34775ea02d6fd16f5618deef
WanqingYang <latejean@gmail.com>
2016-10-11 00:33:08 -0700

    [378. Kth Smallest Element in a Sorted Matrix][Accepted]committed by LeetCode Extension

---
+++ 378-Kth-Smallest-Element-in-a-Sorted-Matrix/solution.cpp
@@ -0,0 +1,25 @@
+/**every time calculate the elements less or equal to mid
+ **/
+class Solution {
+public:
+    int kthSmallest(vector<vector<int>>& matrix, int k) {
+        int n = matrix.size();
+        int start = matrix[0][0], end = matrix[n-1][n-1];
+        while(start < end) {
+            int mid = start + (end - start) / 2;
+            int cnt = 0, j = n - 1;
+            for(int i = 0; i < n; i++) {
+                while(j >=0 && matrix[i][j] > mid) {
+                    j--;
+                }
+                cnt += j+1;
+            }
+            if(cnt < k) {
+                start = mid + 1;
+            } else {
+                end = mid;
+            }
+        }
+        return start;
+    }
+};
